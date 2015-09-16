d35cc4d791943585759938d95dd08fa62fc79768
Tim Krofecheck <tkrofecheck@webmd.net>
2015-09-16 16:50:45 -0400

    funded 'up next' redesign (updated: xsl, css, js)

--- src/js/amd/funded-nextup.js
+++ src/js/amd/funded-nextup.js
@@ -6,53 +6,39 @@ if(!webmd){
 
 webmd.fundedEditorial.nextUp = {
 
-	articles_to_display: 3, 				// number of articles to display in module
-	is_current_sponsored: false,			// flag for determinging if current page is sponsored
-	hide_sponsor_pages: false, 				// disable sponsor pages within the navigation (this will be configured globally within webmd.fundedEditorial and can be removed)
-	hide_on_sponsored: false,				// do not display paddles on sponsored pages
+	articles_to_display: 4, 				// number of articles to display in module
 	article_ids_to_display: [],
-	nextup_article_data: {"articles":[]},
+	article_data: {"articles":[]},
 
 	init : function(){
+		this.setSegmentTitle();
+
+		if (this.articles_to_display > 2) {
+			this.setSeeAllLink();
+		}
+
+		this.getArticleLinks();
 		this.render();
 	},
 
-	injectHBtemplateJS : function() { // inject embedded script to reduce http calls
-		var self = this,
-			$script = $('<script></script>'), 
-			newline = '\n'; //allows readability using inspector
+	setSegmentTitle : function() {
+		var segmentTitle = webmd.fundedEditorial.articleData.program.title;
 
-		$script
-			.attr({
-				id   : 'funded-nextup',
-				type : 'text/x-handlebars-template'
-			})
-			.html(
-				'{{#nextup_article_data}}' + newline +
-				'<ul class="article-list">' + newline +
-					'<li class="up-next-header">Up Next</li>' + newline +
-					'<li>' + newline +
-						'<ul class="articles">' + newline +
-						'{{#each articles}}' + newline +
-							//'<li class="{{#if article.visited}}visited{{/if}}">' + newline +
-							'<li>' + newline +
-								'<a href="{{article.link}}">' + newline +
-									'<span class="text">' + newline +
-										'<span class="title">{{article.title}}</span>' + newline +
-									'</span>' + newline +
-								'</a>' + newline +
-							'</li>' + newline +
-						'{{/each}}' + newline +
-						'</ul>' + newline +
-					'</li>' + newline +
-				'</ul>' + newline +
-				'{{/nextup_article_data}}'
-			);
-
-		$('head').append($script);
+		$('.article-list-container > .wbmd-title').html(segmentTitle);
 	},
 
-	setNextUpArticles : function() {
+	setSeeAllLink : function() {
+		var currentURL = window.location.href.split("?")[0].split("#")[0],
+			$seeAllContainer = $('.article-list-container > .wbmd-see-all'),
+			$a = $('<a></a>'),
+			href = currentURL.split('/').slice(0, -1).join('/') + '/see-all-page';
+
+		$a.attr({ href : href }).html("SEE ALL");
+
+		$seeAllContainer.append($a);
+	},
+
+	getArticleLinks : function() {
 		var self = this,
 			article_data = webmd.fundedEditorial.articleData,
 			articles = article_data.articles,
@@ -64,15 +50,9 @@ webmd.fundedEditorial.nextUp = {
 			articleIndex = articles.indexOf(article);
 			articleIdArrLen = self.article_ids_to_display.length;
 
-			if ((articleIndex > caIndex) &&
-				(articleIdArrLen < self.articles_to_display)) {
-				if (self.hide_sponsor_pages) {
-					if (!articles[key].sponsored) {
-						self.nextup_article_data.articles.push({"article" : article});
-						self.article_ids_to_display.push(articleIndex);
-					}
-				} else {
-					self.nextup_article_data.articles.push({"article" : article});
+			if (!article.sponsored) {
+				if ((articleIndex > caIndex) && (articleIdArrLen < self.articles_to_display)) {
+					self.article_data.articles.push({"article" : article});
 					self.article_ids_to_display.push(articleIndex);
 				}
 			}
@@ -92,17 +72,10 @@ webmd.fundedEditorial.nextUp = {
 				articleIndex = articles.indexOf(article);
 				articleExists = (self.article_ids_to_display.indexOf(articleIndex) != -1);
 
-				if (!articleExists && (self.article_ids_to_display.length < self.articles_to_display)) {
-					if (self.hide_sponsor_pages) {
-						if (!article.sponsored && !article.isCurrent) {
-							self.nextup_article_data.articles.push({article : article});
-							self.article_ids_to_display.push(articleIndex);
-						}
-					} else {
-						if (!article.isCurrent) {
-							self.nextup_article_data.articles.push({article : article});
-							self.article_ids_to_display.push(articleIndex);
-						}
+				if (!article.sponsored && !articleExists && (self.article_ids_to_display.length < self.articles_to_display)) {
+					if (!article.isCurrent) {
+						self.article_data.articles.push({article : article});
+						self.article_ids_to_display.push(articleIndex);
 					}
 				}
 			}
@@ -111,49 +84,72 @@ webmd.fundedEditorial.nextUp = {
 		}
 	},
 
-	bindEvents : function() {
-		var self = this;
-
-		$('.articles li a').hover( // hide 2px border when hovering (remove if hover is not used)
-			function() {
-				$(this).closest("li").addClass("no-bottom-border");
-				$(this).closest("li").prev().addClass("no-bottom-border");
-			}, function() {
-				$(this).closest("li").removeClass("no-bottom-border");
-				$(this).closest("li").prev().removeClass("no-bottom-border");
-			}
-		);
-	},
-
-	render : function(){ // uses handlebars template above
+	render: function() {
+		console.log('render');
 		var self = this,
-			next_up_articles_len;
+			$container = $(".article-list-container > .wbmd-nav-links"),
+			links;
 
-		if (self.hide_on_sponsored && self.is_current_sponsored) {
-			return true;
-		} else {
-			self.setNextUpArticles();
+		links = createArticleLinkNodes();
 
-			next_up_articles_len = self.nextup_article_data.articles.length;
+		$.each(links, function() {
+			$container.append(this);
+		});
 
-			// Do no create module if not enough articles in data object
-			if (next_up_articles_len > 0) {
-				self.injectHBtemplateJS();
+		// Do no create module if not enough articles in data object
+		
+		function createArticleLinkNodes() {
+			var articles_len = self.article_data.articles.length,
+				nodes = [],
+				article,
+				articleType,
+				$div,
+				$type,
+				$link;
 
-				require(["handlebars/1/handlebars"], function(Handlebars) {
-					var $template = $("#funded-nextup"),
-						$container = $(".article-list-container"),
-						$articles = $(".articles"),
-						source = $template.html(),
-						template = Handlebars.compile(source),
-						context = {"nextup_article_data" : self.nextup_article_data} || {},
-						html = template(context);
-
-					$container.prepend(html);
-
-					self.bindEvents();
-				});
+			for (var i=0; i<articles_len; i++) {
+				article = self.article_data.articles[i].article;
+				articleType = getType(self.article_data.articles[i].article.type);
+				$div = $('<div></div>');
+				$type = $('<div></div>');
+				$type.addClass('wbmd-type').html(articleType);
+				$link = $('<a></a>');
+				$link.attr({ href : article.link });
+				$link.html(article.title);
+				$div.append($type).append($link);
+				nodes.push($div);
 			}
+
+			return nodes;
+		}
+
+		function getType(type) {
+			var val = '';
+
+			switch (type) {
+				case "type_art":
+					val = "Article";
+					break;
+				case "type_ss":
+					val = "Slideshow";
+					break;
+				case "type_toc":
+					val = "Destination";
+					break;
+				case "type_com":
+					val = "Blog";
+					break;
+				case "type_vid":
+					val = "Video";
+					break;
+				case "type_rmq":
+					val = "Quiz";
+					break;
+				default:
+					break;
+			}
+
+			return val;
 		}
 	}
 };
--- src/xslt/nextup/nextup.xsl
+++ src/xslt/nextup/nextup.xsl
@@ -15,7 +15,33 @@
             <xsl:attribute name="class">
                 <xsl:text>article-list-container</xsl:text>
             </xsl:attribute>
+        
+            <xsl:element name="div">
+                <xsl:attribute name="class">
+                    <xsl:text>wbmd-brand</xsl:text>
+                </xsl:attribute>
+                <xsl:text><![CDATA[FROM WebMD]]></xsl:text>
+            </xsl:element>
+        
+            <xsl:element name="div">
+                <xsl:attribute name="class">
+                    <xsl:text>wbmd-title</xsl:text>
+                </xsl:attribute>
+            </xsl:element>
+            
+            <xsl:element name="div">
+                <xsl:attribute name="class">
+                    <xsl:text>wbmd-nav-links</xsl:text>
+                </xsl:attribute>
+            </xsl:element>
+        
+            <xsl:element name="div">
+                <xsl:attribute name="class">
+                    <xsl:text>wbmd-see-all</xsl:text>
+                </xsl:attribute>
+            </xsl:element>
         </xsl:element>
+        
         <xsl:element name="script">
         <![CDATA[require(["funded-editorial/1/funded-nextup"]);]]>
         </xsl:element>
