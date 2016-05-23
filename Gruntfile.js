172450b38cfce990e6162b582b707cf1968d0775
Lu, Ryan (Ryan Lu) <rlu@webmd.net>
2016-05-23 13:46:11 -0400

    updates to attribution and disclaimer modules for site

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
 		autoprefixer: {
 			dist: {
 				options: {
-					browsers: ['last 2 versions', 'ie 8', 'ie 9']
+					browsers: ['last 2 versions', 'ie 8', 'ie 9', 'iOS 7']
 				},
 				src: '<%= dirBuild %>/**/*.css'
 			}
