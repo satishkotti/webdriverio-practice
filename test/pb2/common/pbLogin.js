06de53471d785d62bca0a3dee9ca9af13771e0bc
npshah <npshah@webmd.net>
2017-02-19 08:09:11 -0500

    init

---
+++ test/pb2/common/pbLogin.js
@@ -0,0 +1,25 @@
+var page = require('./../../common/page')
+
+var testUrl = 'http://genesys.dev01.webmd.com';
+
+var pbLoginPg = Object.create(page, {
+    
+    username: { get: function () { return browser.element('#username'); } },
+    password: { get: function () { return browser.element('#password'); } },
+    form:     { get: function () { return browser.element('#pb-login'); } },
+    title:    { get: function () { return browser.getTitle(); } },
+    failLoginText: { get: function () {
+        return browser.getText("span=Username or password incorrect");
+    } },
+    browser: { get: function()  { return browser }},
+
+    open: { value: function() {
+        page.open.call(this, testUrl);
+    } },
+
+    submit: { value: function() {
+        this.form.submitForm();
+    } }
+});
+
+module.exports = pbLoginPg
