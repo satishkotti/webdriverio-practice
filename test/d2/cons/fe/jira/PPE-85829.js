82205deb32b217816012c8cb55a12d7ff31b0f8d
WBMD\njain <njain@webmd.net>
2017-03-21 10:38:42 -0400

    ++ insert pull quote, power promote, publish to staging/active, xml verification PPE-85829

--- test/d2/cons/fe/jira/PPE-85829.js
+++ test/d2/cons/fe/jira/PPE-85829.js
@@ -1,3 +1,4 @@
+var Promise = require('bluebird');
 var functions = require('./../../common/functions/functions');
 var Login = require('./../../common/actions/login.actions');
 var repositoryBrowserTab = require('./../../common/actions/repositoryBrowserTab.actions');
@@ -6,22 +7,23 @@ var documentListTab = require('./../../common/actions/documentListTab.actions');
 var contentTab = require('./../../common/actions/contentTab.actions');
 var ckEditorMenu = require('./../../common/actions/ckEditor.actions.js');
 var mModulePullQuote = require('./../../common/actions/mModulePullQuote.actions.js');
+var propertiesTab = require('./../../common/actions/propertiesTab.actions');
+var documentListTab = require('./../../common/actions/documentListTab.actions');
+var JSONPath = require('JSONPath');
+var parseXml = require('./../../common/components/parseXml');
 
 describe('mModule Pull Quote', function () {
-    
+    var chronicleId;
+    var jsCodeValue;
+
     before(function () {
-
-        browser.setViewportSize({
-            width: 1920,
-            height: 1080
-        });
-
+        browser.windowHandleMaximize();
         Login.login({
             url: functions.getEnvTestUrl(),
             username: functions.getQAPublicationUser().username,
             password: functions.getQAPublicationUser().password
         });
-
+        
         repositoryBrowserTab.openFolder(global.d2ConDataSettings.inputData.testFolderPath);
         workspaceMenu.createContent(
                 global.d2ConDataSettings.inputData.ArticleProfileName,
@@ -31,6 +33,14 @@ describe('mModule Pull Quote', function () {
         
         documentListTab.selectAsset(global.d2ConDataSettings.inputData.ArticleObjectName);
         contentTab.checkOut();
+        jsCodeValue = 'sample test data';
+    });
+
+    it('should verify that clicking cancel button does not insert pull quote', function () {
+        contentTab.sectionTextSetValue("sample test data");
+        ckEditorMenu.sectionTextPullQuoteMenuClick();
+        mModulePullQuote.addQuoteTextAttributeAlignCancel(global.d2ConDataSettings.inputData.PullQuoteText,
+        global.d2ConDataSettings.inputData.PullQuoteAttribution, global.d2ConDataSettings.inputData.PullQuoteAlignLeft);
     });
 
     it('should select section text then add pull quote', function () {
@@ -39,6 +49,41 @@ describe('mModule Pull Quote', function () {
         mModulePullQuote.addQuoteTextAttributeAlignInsert(global.d2ConDataSettings.inputData.PullQuoteText,
         global.d2ConDataSettings.inputData.PullQuoteAttribution, global.d2ConDataSettings.inputData.PullQuoteAlignLeft);
         contentTab.checkIn();
+        
+    });
+
+    it('should promote and publish', function () {
+        var cidName = propertiesTab.getChronicleIdAndName();
+        var objName = cidName.objectName;
+        chronicleId = cidName.chronicleId;
+        propertiesTab.setRequiredProperties(objName,'ZZ - Dummy Content Classification',objName,objName,objName,objName,'No URL dummy publication','2015 WebMD','Cold and Flu')
+        documentListTab.assetPowerPromotePublishToStaging(global.d2ConDataSettings.inputData.ArticleObjectName);
+    });
+
+        // it('should select section text then update pull quote', function () {
+        // browser.doubleClick(".widgetToDelete");
+        // mModulePullQuote.addQuoteTextAttributeAlignInsert('test1',
+        // 'test2', 'global.d2ConDataSettings.inputData.PullQuoteAlignLeft');
+        // contentTab.checkIn();
+        // });
+    it('should be part of scs rendition', function () {
+         return Promise.resolve(
+            parseXml.getXmlFromUrl(functions.getAtsScsFileUrl()+chronicleId, null).then(function (result) {
+                var expectedJsCode = ' '+jsCodeValue;
+                var jsEmbedAssets = JSONPath({json: result,  path: "$..section_text.embeded_module[?(@.type =='pullquote')]", resultType: 'all' });
+                expect(jsEmbedAssets[0].parent.$.align).to.equal('left');
+                expect(jsEmbedAssets[0].parent.$.class).to.equal('wbmdembededmodule cke_widget_inline');
+                expect(jsEmbedAssets[0].parent.$.module_description).to.equal('QA Pull Quote Attribution');
+                expect(jsEmbedAssets[0].parent.$.module_title).to.equal('QA Pull Quote Text');
+                expect(jsEmbedAssets.length).to.equal(1);
+                //expect(jsEmbedAssets.length).to.equal(1);
+                //expect(jsEmbedAssets[0].parent.$.asset_description).to.equal('undefined');
+                //expect(jsEmbedAssets[0].parent.$.asset_title).to.equal('undefined');
+                //expect(jsEmbedAssets[0].parent.$.chronic_id).to.equal('undefined');
+                //expect(jsEmbedAssets[0].parent.$.thumbnail).to.equal('undefined');
+                //var jsBlobVal = JSONPath({json: result,  path: "$..section_text.embeded_module.jsblob" });
+                //expect(jsBlobVal[0]).to.equal(expectedJsCode);
+        }));  
     });
 
 });
