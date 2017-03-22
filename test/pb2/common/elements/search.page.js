7eb0c6886e90d8855e20532ea56a9778f1662f9f
Sandeep Putla <sandelirious7@gmail.com>
2017-03-22 12:14:03 +0530

    modified code for searching shared modules

--- test/pb2/common/elements/search.page.js
+++ test/pb2/common/elements/search.page.js
@@ -11,7 +11,7 @@ var search = Object.create(page, {
         switch(category)
         {
             case 'DPM': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 'm'); browser.waitForExist(locator); browser.click(tabHeading.replace('***', category)); break;
-            case 'SM': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 'm'); browser.waitForExist(locator); browser.click(tabHeading.replace('***', category)); break;
+            case 'SM': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 'm'); browser.waitForExist('(' + locator + ')[2]'); browser.click(tabHeading.replace('***', category)); break;
             case 'XSL': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 'x'); browser.waitForExist(locator); browser.click(tabHeading.replace('***', category)); break;
             case 'Template': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 't'); browser.waitForExist(locator); browser.click(tabHeading.replace('***', category)); break;
             case 'Page': locator = tabHeading.replace('***', category) + spinnerLoc.replace('*', 'p'); browser.waitForExist(locator); browser.click(tabHeading.replace('***', category)); break;
