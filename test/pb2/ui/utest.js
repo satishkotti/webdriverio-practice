57fe111873bab0b47e5635c9c972b1b2f79f296c
Sandeep Putla <sandelirious7@gmail.com>
2017-03-23 15:48:14 +0530

    + code to compare date time using moment-js

--- test/pb2/ui/utest.js
+++ test/pb2/ui/utest.js
@@ -4,34 +4,36 @@ var pageTestData = require('./../data/page.assets');
 var templateTestData = require('./../data/template.assets');
 var smTestData = require('./../data/sm.assets');
 var pmTestData = require('./../data/pagemodule.assets');
+var momentTz = require('moment-timezone');
+var moment = require('moment');
 
 describe('Unit Tests', () => {
 
-    it('Launch App and login', () => {
+    it.skip('Launch App and login', () => {
         func.LaunchAppAndLogin();
     });
 
-    it('Enter Interior Workcenter', () => {
+    it.skip('Enter Interior Workcenter', () => {
         func.EnterIWC('Create', 'Templates & Pages');
     });
 
-    it('Traverse Site Structure', () => {
+    it.skip('Traverse Site Structure', () => {
         func.TraverseSS('Level 0/zTest/zSubTest1');
     });
 
-    it('Add Normal Standalone Page to the Node', () => {
+    it.skip('Add Normal Standalone Page to the Node', () => {
         func.Create('Page', pageTestData.normalStandalonePage);
     });
 
-    it('Add a page module', () => {
+    it.skip('Add a page module', () => {
         func.AddModule('ContentPane0', pmTestData.adModule);
     });
 
-    it('Add another page module', () => {
+    it.skip('Add another page module', () => {
         func.AddModule('ContentPane1', pmTestData.adModule);
     });
 
-    it('Publish the page to Live', () => {
+    it.skip('Publish the page to Live', () => {
         func.SaveOrPublishTheAsset('Save', 'Test');
     });
 
@@ -58,5 +60,24 @@ describe('Unit Tests', () => {
         func.SearchFor(null, '02QATestPage002', 'Interior Workcenter', 'Level 0/zTest/zSubTest1');
         expect(browser.elements('//tr[@aria-selected="true"]//td[contains(.,"02QATestPage002")]').value.length).to.equal(1);
     });
+
+    it('Working with timezones', () => {
+        var ind = momentTz().tz('Asia/Kolkata');
+        var indFormatted = ind.format('MMM DD, YYYY h:mm:ss A');
+        var nyc = ind.clone().tz('America/New_York');
+        var nycFormatted = ind.clone().tz('America/New_York').format('MMM DD, YYYY h:mm:ss A');
+
+        var nycTestTime = momentTz('Mar 23, 2017 5:57:18 AM');
+        var nycTestTimeFormatted = momentTz('Mar 23, 2017 5:57:18 AM').format('MMM DD, YYYY h:mm:ss A');
+
+        console.log('IST: ' + indFormatted);
+        console.log('EST: ' + nycFormatted);
+        console.log('EST: ' + nycTestTimeFormatted);
+        //console.log(ind.isBefore(nyc));
+        console.log(nyc);
+        console.log(nycTestTime);
+        console.log(nycTestTime < nyc);
+        //console.log(indFormatted < nycFormatted);
+    })
     
 });
