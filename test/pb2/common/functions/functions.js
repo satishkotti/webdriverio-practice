dfca857fdc5c041de91a2858b9a7f27bccd23ae2
Sandeep Putla <sandelirious7@gmail.com>
2017-03-29 19:11:06 +0530

    added comments for functions

--- test/pb2/common/functions/functions.js
+++ test/pb2/common/functions/functions.js
@@ -8,6 +8,7 @@ var queue = require('./../actions/queue.actions');
 var usersDetails = require('./../../config/users');
 var user = usersDetails.users;
 
+//Launch App and login
 module.exports.LaunchAppAndLogin = (user) =>
 {
     switch (user)
@@ -19,6 +20,7 @@ module.exports.LaunchAppAndLogin = (user) =>
     
 }
 
+//Enter credentials in the login screen and click on the login button
 module.exports.Login = (user) => {
     switch (user)
     {
@@ -28,16 +30,24 @@ module.exports.Login = (user) => {
     }
 }
 
+//Enter Interior Workcenter
 module.exports.EnterIWC = (menu, option) => 
 {
     iwc.EnterIWC(menu, option);
 
 }
 
+//Traverse the Site Structure. Level must be mentioned from the root node. Eg: 'Level 0/zTest/QA & Dev'
 module.exports.TraverseSS = (level) =>
 {
     iwc.TraverseSS(level);
 }
+
+/*
+Create a page or template or shared module.
+'assetType' can be 'Page' or 'Template' or 'Shared Module'
+'assetDetails' can be json from pb2/data/****.assets.js
+*/
 
 module.exports.Create = (assetType, assetDetails) =>
 {
@@ -49,12 +59,25 @@ module.exports.Create = (assetType, assetDetails) =>
     }
 }
 
+/*
+Add a page or template.
+'contentPane' is the name of the content pane. Eg: ContentPane0
+'moduleDetails' can be json from pb2/data/****.assets.js
+*/
 module.exports.AddModule = (contentPane, moduleDetails) =>
 {
     props.AddModule(contentPane, moduleDetails);
 }
 
-module.exports.SearchFor = (assetType, keyword, from, level) => // assetType can have 'Page', 'Template', 'SM', 'DPM'. keyword can be Chronicle ID or keyword like test
+/*
+Search for an asset using either Global Search or Site Structure
+'assetType' can have 'Page', 'Template', 'SM', 'DPM'. (can be null if chronicle id is passed as argument for 'keyword')
+'keyword' can be Chronicle ID or keyword like test
+'from' can be 'Global Search' or 'Interior Workcenter'
+'level' is the level to traverse in the Site Structure from the root node (can be as null if Global Search is used)
+*/
+
+module.exports.SearchFor = (assetType, keyword, from, level) =>
 {
     switch (from)
     {
@@ -77,26 +100,49 @@ module.exports.SearchFor = (assetType, keyword, from, level) => // assetType can
     }
 }
 
+//Edit the asset if 'Edit' button is displayed in the screen
 module.exports.EditTheAsset = () =>
 {
     act.ClickEditButton();
 }
 
+//Edit the asset if 'Checkout & Edit' button is displayed in the screen
 module.exports.CheckoutAndEditTheAsset = () =>
 {
     act.ClickCheckoutAndEditButton();
 }
 
-module.exports.SaveOrPublishTheAsset = (action, comment) =>
-{
-    act.SavePublish(action, comment);
-}
-
+//Switch between tabs while editing the asset. eg for 'tabName': Preview
 module.exports.SwitchAssetTabs = (tabName) =>
 {
     props.SwitchAssetTabs(tabName);
 }
 
+/*
+Checkin or Publish the asset
+'action' can be 'Checkin' (or 'Save') or 'Publish to Staging' or 'Publish to Live'
+'comment' can be user comment entered while saving/publishin the asset
+*/
+module.exports.SaveOrPublishTheAsset = (action, comment) =>
+{
+    act.SavePublish(action, comment);
+}
+
+/*
+Publish the asset from More Actions menu
+'action' can be 'Publish to Staging' or 'Publish to Live'
+'comment' can be user comment entered while saving/publishin the asset
+*/
+module.exports.SaveOrPublishTheAssetFromMoreActions = (action, comment) =>
+{
+    act.SavePublishFromMoreActions(action, comment);
+}
+
+/*
+Obtain the asset version (eg: 3.0) and stage (eg: staging)
+'assetName' can be 'selected' (if the asset is selected in Interior Workcenter or Search Results screen) or can be null if user is in asset readonly/edit mode.
+'from' can be 'Asset Screen' (if user is in asset readonly/edit mode) or null
+*/
 module.exports.GetAssetVersionAndStage = (assetName, from) =>
 {
     switch(from)
@@ -106,82 +152,118 @@ module.exports.GetAssetVersionAndStage = (assetName, from) =>
     }
 }
 
+//Enter the activity queue
 module.exports.EnterActivityQueueStatusPage = () =>
 {
     menu.EnterActivityQueueStatusPage();
 }
 
+//Filter the results in the activity queue. 'filter' can be 'Áll', 'Completed', 'Failed', 'In Progress'
 module.exports.FilterResultsInQueue = (filter) =>
 {
     queue.Show(filter);
 }
 
+//Gets the latest details of an asset if present in the queue or else return a json with null values
 module.exports.GetAssetDetailsFromQueue = (chronID) =>
 {
     return queue.GetAssetDetailsFromQueue(chronID);
 }
 
+//Refresh the queue by clicking on the 'Refresh' button
 module.exports.RefreshTheQueue = () =>
 {
     queue.RefreshTheQueue();
 }
 
+//Enable or Disable auto refresh functionality in the queue
 module.exports.EnableDisableAutoRefresh = (action) =>
 {
     queue.ClickAutoRefresh(action);
 }
 
+//Returns the current site being tested. eg: WebMD Desktop
 module.exports.GetCurrentSite = () =>
 {
     return app.GetCurrentSite();
 }
 
+//Returns the Chronicle ID of the selected asset. 'screen' can be 'IWC' (uses IWC even if null is mentioned) or 'Search Results'
 module.exports.GetChronIDOfTheSelectedAsset = (screen) =>
 {
     return iwc.GetChronIDOfTheSelectedAsset(screen);
 }
 
-module.exports.SaveOrPublishTheAssetFromMoreActions = (action, comment) =>
-{
-    act.SavePublishFromMoreActions(action, comment);
-}
-
+//Selects the required asset from the list. 'assetName' is the name of the asset to be selected
 module.exports.SelectAsset = (assetName) =>
 {
     iwc.SelectAsset(assetName);
 }
 
+//Navigates to the homepage
 module.exports.NavigateToHomepage = () =>
 {
     menu.GoHome();
 }
 
+/*
+Sorts the required column in the mentioned table
+'table' can be the name of the table. eg: Checked Out Objects
+'column' can be the column to be sorted. eg: Last Modified
+'sortType' can be Ascending or Descending
+*/
 module.exports.SortTableColumn = (table, column, sortType) =>
 {
     menu.SortColumn(table, column, sortType);
 }
 
+//Select a More Actions menu-item. eg: Asset History
 module.exports.SelectMoreActionsMenuItem = (menuItem) =>
 {
     act.SelectMoreActionsMenuItem(menuItem);
 }
 
+//Clicks on Show Expired button in IWC or Search Results screen
 module.exports.ClickShowExpired = () =>
 {
     act.ClickShowExpired();
 }
 
+
+/*
+Propagates templates modules. 'specs' is a json.
+eg for spec:
+{
+    "modules":
+        [
+            {
+                "moduleName": "PM0004",
+                "modulePosition": "Top of Content Pane" 
+            },
+            {
+                "moduleName": "PM0006",
+                "modulePosition": "Bottom of Content Pane" 
+            }
+        ],
+        "childTemplates": ['Test Template', 'Test Template2'],
+        "childPageStage": "Live"
+}
+*/
 module.exports.Propagate = (specs) =>
 {
     props.Propagate(specs);
 }
 
+//Cancel's the checkout on an asset
 module.exports.CancelCheckout = () =>
 {
     act.CancelCheckout();
 }
 
-module.exports.SelectNodeAction=([actionName, publishNode,templateStatus,pageStatus])=>
+/*
+
+*/
+module.exports.SelectNodeAction = ([actionName, publishNode,templateStatus,pageStatus]) =>
 {
      act.SelectNodeAction([actionName, publishNode,templateStatus,pageStatus]);
 }
--- wdio.d2.conf.js
+++ wdio.d2.conf.js
@@ -1,7 +1,47 @@
 var Q = require("q");
-exports.config = {
-    debug: true,
 
+module.exports.getSpecs = function()
+{
+    var specList;
+    var testApp = process.env.npm_config_testApp ? process.env.npm_config_testApp : 'd2cons' ;
+    try
+    {
+        switch(testApp) {
+            case "pb2":
+                var config = require('./test/pb2/config/config');
+                specList = [ './test/pb2/**/*.js' ];
+            break;
+            case "d2cons":
+                var config = require('./test/d2/cons/config/config');
+                specList = [ './test/d2/cons/**/*.js' ];
+            break;
+            case "d2prof":
+                var config = require('./test/d2/prof/config/config');
+                specList = [ './test/d2/prof/**/*.js' ];
+            break;
+            case "rt":
+                var config = require('./test/rt/config/config');
+                specList = [ './test/rt/**/*.js' ];
+                break;
+            default:
+                throw "Missing Specs"
+            }  
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+
+console.log('specs: '+specList);
+
+        return specList;
+};
+
+exports.config = {
+
+    debug: false,
+    maxInstances: 10,
+    
     //
     // ==================
     // Specify Test Files
@@ -11,11 +51,11 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    spec:['./test/pb2/**.js'],
-    
+    specs: module.exports.getSpecs(),
     // Patterns to exclude.
     exclude: [
-        // 'path/to/excluded/files'
+         './test/d2/cons/config/**/*.*',
+         './test/d2/cons/common/**/*.*',
     ],
     //
     // ============
@@ -31,13 +71,7 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
-        maxInstances: 1,
-        browserName: 'chrome',
-        chromeOptions:
-        {
-            //args: ['window-size=1920,1080']
-            args:['start-maximized', 'disable-infobars']
-        }
+        browserName: 'chrome'
     }],
     //
     // ===================
@@ -47,7 +81,7 @@ exports.config = {
     //
     // Level of logging verbosity: silent | verbose | command | data | result | error
     logLevel: 'error',
-    //
+    //log
     // Enables colors for log output.
     coloredLogs: true,
     //
@@ -59,7 +93,7 @@ exports.config = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitForXXX commands.
-    waitforTimeout: 60000,
+    waitforTimeout: 120000,
     //
     // Initialize the browser instance with a WebdriverIO plugin. The object should have the
     // plugin name as key and the desired plugin options as property. Make sure you have
@@ -105,7 +139,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 3000000
+        timeout: 1200000
     },
 
     //
@@ -138,8 +172,10 @@ exports.config = {
     // Gets executed before test execution begins. At this point you will have access to all global
     // variables like `browser`. It is the perfect place to define custom commands.
     before: function() {
+
+        var testEnv = (process.env.npm_config_testEnv) ? process.env.npm_config_testEnv : 'dev04';
+        var testApp = process.env.npm_config_testApp ? process.env.npm_config_testApp : 'd2cons' ;
         
-        var testEnv = (process.env.npm_config_testEnv) ? process.env.npm_config_testEnv : 'dev01';
         var chai = require('chai');
         chai.config.includeStack = true;
         expect = chai.expect;
@@ -148,7 +184,39 @@ exports.config = {
         assert = chai.assert;
         should = chai.should();
         _ = require('lodash');
-        
+
+    try
+    {
+        switch(testApp) {
+            case "pb2":
+                var config = require('./test/pb2/config/config');
+                global.envSettings = config.EnvSettings.getEnvSettings(testEnv);
+                global.dataSettings = config.EnvSettings.getEnvData(testEnv);
+            break;
+            case "d2cons":
+                var config = require('./test/d2/cons/config/config');
+                global.envSettings = config.EnvSettings.getEnvSettings(testEnv);
+                global.d2ConDataSettings = config.EnvSettings.getEnvData(testEnv);
+            break;
+            case "d2prof":
+                var config = require('./test/d2/prof/config/config');
+                global.envSettings = config.EnvSettings.getEnvSettings(testEnv);
+                global.d2ProfDataSettings = config.EnvSettings.getEnvData(testEnv);
+            break;
+            case "rt":
+                var config = require('./test/rt/config/config');
+                global.envSettings = config.EnvSettings.getEnvSettings(testEnv);
+                global.rt2DataSettings = config.EnvSettings.getEnvData(testEnv);
+                break;
+            default:
+                specs = [ ];
+            }  
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+
     },
     //
     // Gets executed after all tests are done. You still have access to all global variables from
@@ -161,5 +229,23 @@ exports.config = {
     // possible to defer the end of the process using a promise.
     onComplete: function() {
         // do something
+    },
+    suites: {
+        pb2Sanity: [
+            './test/pb2/sanity/favorite.js',
+            './test/pb2/sanity/page.js',
+            './test/pb2/sanity/template.js'
+            ],
+        pb2Ui: [
+            './test/pb2/ui/login.js',
+            './test/pb2/ui/navmap.js',
+            './test/pb2/ui/ppe-81340.js'
+            ],
+        rtSanity: [ 
+            './test/rt/sanity/dynamicUrl.js',
+            './test/rt/sanity/homePage.js'
+            ],
+        rtUi:[
+            ],
     }
 };
