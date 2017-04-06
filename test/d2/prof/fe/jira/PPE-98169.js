3fe481d48391a7d571d4c07e4c2720a28dc06e98
Murali Mohan Reddy Eevuri <murali.eevuri@lsz.ads.valuelabs.net>
2017-04-06 18:03:47 +0530

    ++98169- pull request comments implemented

--- test/d2/prof/fe/jira/PPE-98169.js
+++ test/d2/prof/fe/jira/PPE-98169.js
@@ -20,9 +20,7 @@ describe('News Team - Customization Template PPE-98169', function () {
 
     var  newsObjectname= global.d2ProfDataSettings.inputData.NewsArticleObjectName;
     var objectName=global.d2ProfDataSettings.inputData.ArticleObjectName;
-    console.log("obj1" + newsObjectname);
-    console.log("obj1" + objectName);
-    it('Verify the News article creation with a user in News group', function () {
+    it('Shouldbe able to create the News article with a user in News group', function () {
        
         workspaceMenu.createContent(global.d2ProfDataSettings.inputData.ArticleProfileName,
                     global.d2ProfDataSettings.inputData.ArticleTemplate, 
@@ -37,7 +35,7 @@ describe('News Team - Customization Template PPE-98169', function () {
         expect(newsObjectname).to.equal(title);
     });
 
-    it('Verify the properties screen of News article with a user who belongs to News group', function () {
+    it('Should display the properties of News article for user that belongs to News group PPE-98169', function () {
         browser.pause(2000);
         var isexits="";
         documentListTab.selectAsset(newsObjectname);
@@ -47,27 +45,27 @@ describe('News Team - Customization Template PPE-98169', function () {
         expect(data).to.equal("None");
     });
 
-    it('Verify the properties screen of News article with a user who belongs to Non-News group', function () {
+    it('Should not display properties of News article for user that do not belong to Non-News group PPE-98169', function () {
         Login.logout();
         Login.login({
         url: functions.getEnvTestUrl(),
         username: functions.getQAPublicationUser().username,
         password: functions.getQAPublicationUser().password
-    });
-    
-       repositoryBrowserTab.openFolder(global.d2ProfDataSettings.inputData.testFolderPath);
+        });
+
+        repositoryBrowserTab.openFolder(global.d2ProfDataSettings.inputData.testFolderPath);
         workspaceMenu.createContent(global.d2ProfDataSettings.inputData.ProfileName,
-                    global.d2ProfDataSettings.inputData.ArticleTemplate, 
-                    objectName, 
-                    global.d2ProfDataSettings.inputData.ContentType);
+        global.d2ProfDataSettings.inputData.ArticleTemplate, 
+        objectName, 
+        global.d2ProfDataSettings.inputData.ContentType);
         var cidName = propertiesTab.getChronicleIdAndName();
         var objName = cidName.objectName;
         var title = cidName.title;
         propertiesTab.setRequiredProperties(objName,objName,objName,global.d2ProfDataSettings.inputData.LeadSpecialty,
-                    global.d2ProfDataSettings.inputData.ContentDeveloper);
+        global.d2ProfDataSettings.inputData.ContentDeveloper);
 
-       var basicInfo=propertiesTab.verifyBasciInfoTabProperties(global.d2ProfDataSettings.inputData.basicInfoTab.split(','));
-       var articleinfo =propertiesTab.verifyArticleTabProperties(global.d2ProfDataSettings.inputData.articleTab.split(','));
+        var basicInfo=propertiesTab.verifyBasciInfoTabProperties(global.d2ProfDataSettings.inputData.basicInfoTab.split(','));
+        var articleinfo =propertiesTab.verifyArticleTabProperties(global.d2ProfDataSettings.inputData.articleTab.split(','));
         expect(objectName).to.equal(title);
         expect(basicInfo).to.equal("");
         expect(articleinfo).to.equal("");
