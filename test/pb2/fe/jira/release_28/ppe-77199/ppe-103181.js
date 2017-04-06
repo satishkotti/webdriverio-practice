6ee76850b01301cd8eee4cac8d5946af912aff52
pmadamsetti <pmadamsetti@webmd.net>
2017-04-06 14:27:31 +0530

    + code for PPE-103181

--- test/pb2/fe/jira/release_28/ppe-77199/ppe-103181.js
+++ test/pb2/fe/jira/release_28/ppe-77199/ppe-103181.js
@@ -4,16 +4,19 @@ var pageTestData = require('./../../../../data/page.assets');
 var pmTestData = require('./../../../../data/pagemodule.assets');
 
 describe('PPE-77199:Verify the Propagation & Delete Propagation functionality for New Template', () => {
-  var assetDetails = {};
-  var parentTemplateName = '';
-  var childTemplateName = '';
-  var childPageName = '';
+    var assetDetails = {};
+    var parentTemplateName = '';
+    var childTemplateName = '';
+    var childPageName = '';
+    var childPageChronId = '';
+    var childTempChronId = '';
+
     before(() => {
         assetDetails = templateTestData.normalStandaloneTemplate;
         parentTemplateName = assetDetails.templateName;
         var parentTemplateLayout = assetDetails.layout;
         var parentTemplateLayoutCSS = assetDetails.layoutCSS;
-        var templateToInheritFrom = parentTemplateName + ' [' + parentTemplateLayout + 'Layout' + ' - ' + parentTemplateLayoutCSS  + ']';
+        var templateToInheritFrom = parentTemplateName + ' [' + parentTemplateLayout + 'Layout' + ' - ' + parentTemplateLayoutCSS + ']';
 
         test.LaunchAppAndLogin();
         test.EnterIWC('Create', 'Templates & Pages');
@@ -25,18 +28,18 @@ describe('PPE-77199:Verify the Propagation & Delete Propagation functionality fo
         assetDetails = templateTestData.normalInheritedTemplate.get(templateToInheritFrom);
         childTemplateName = assetDetails.templateName;
         test.EnterIWC('Create', 'Templates & Pages');
-        var childTempChronId = test.Create('Template', assetDetails);
+        childTempChronId = test.Create('Template', assetDetails);
         test.AddModule('ContentPane1', pmTestData.htmlModule.get('PM0002'));
         test.SaveOrPublishTheAsset('Publish to Live', 'Testing Activity Queue - Propagate Scenario');
 
         assetDetails = pageTestData.normalInheritedPage.get(templateToInheritFrom);
         childPageName = assetDetails.pageName;
         test.EnterIWC('Create', 'Templates & Pages');
-        var childPageChronId = test.Create('Page', assetDetails);
+        childPageChronId = test.Create('Page', assetDetails);
         test.AddModule('ContentPane2', pmTestData.htmlModule.get('PM0003'));
         test.SaveOrPublishTheAsset('Publish to Live', 'Testing Activity Queue - Propagate Scenario');
 
-        test.SearchFor(null,parentChronId,'Global Search', null);
+        test.SearchFor(null, parentChronId, 'Global Search', null);
         test.EditTheAsset();
         test.SwitchAssetTabs('Template Layout');
         test.AddModule('ContentPane1', pmTestData.htmlModule.get('PM0004'));
@@ -47,31 +50,30 @@ describe('PPE-77199:Verify the Propagation & Delete Propagation functionality fo
             return assetDetails.Status != 'IN PROGRESS';
         }, 120000, "Asset not pushed to the publishing queue yet", 30000);
 
-         test.SearchFor(null,parentChronId,'Global Search', null);
-         test.SwitchAssetTabs('Template Layout');
+        test.SearchFor(null, parentChronId, 'Global Search', null);
+        test.SwitchAssetTabs('Template Layout');
 
-         //Propagate
-         test.Propagate({
+        //Propagate
+        test.Propagate({
             "modules":
             [{
                 "moduleName": "PM0004",
-                "modulePosition": "Top of Content Pane" 
+                "modulePosition": "Top of Content Pane"
             }],
             "childTemplates": [childTemplateName],
             "childPageStage": "Live"
         });
-     
-        //enter into the Queue Page  
-        test.EnterActivityQueueStatusPage();       
 
-        browser.waitUntil( () => 
-        {
+        //enter into the Queue Page  
+        test.EnterActivityQueueStatusPage();
+
+        browser.waitUntil(() => {
             assetDetails = test.GetAssetDetailsFromQueue(parentChronId);
             return assetDetails.Status != 'IN PROGRESS';
         }, 120000, "Asset not pushed to the publishing queue yet", 30000);
 
     });
-   
+
     //assertions
     it('Template Name in the Queue should be same as populated in the Template Properties page', () => {
         expect(assetDetails.Name).to.equal(parentTemplateName);
@@ -80,9 +82,18 @@ describe('PPE-77199:Verify the Propagation & Delete Propagation functionality fo
     it('Template Action should be Publish Template', () => {
         expect(assetDetails.Action).to.equal('Propagate Template Module');
     });
-    
+
     it('Site should be the current Site under test', () => {
         expect(assetDetails.Site).to.equal(test.GetCurrentSite());
     });
-   
+    it('Display of Propagate module on Template', () => {
+        test.SearchFor(null, childTempChronId, 'Global Search', null);
+        test.SwitchAssetTabs('Template Layout');
+        expect(browser.isExisting('//a[@title="PM0004"]')).to.be.true;
+    });
+    it('Display of Propagate module on Page', () => {
+        test.SearchFor(null, childPageChronId, 'Global Search', null);
+        test.SwitchAssetTabs('Page Layout');
+        expect(browser.isExisting('//a[@title="PM0004"]')).to.be.true;
+    });
 });
