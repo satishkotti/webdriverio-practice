5dc84588d9cfa819fd0ea5e5a166049d93ab4c25
Santhosh Kumar Panduga <santhosh.panduga@lsz.ads.valuelabs.net>
2017-05-04 16:09:40 +0530

    PPE-98280 Changed the code as per Nilay Comments.

--- test/d2/prof/fe/jira/profPhase3Sprint1/ppe-98280-DE.js
+++ test/d2/prof/fe/jira/profPhase3Sprint1/ppe-98280-DE.js
@@ -57,7 +57,7 @@ describe('OTF Profoutput-Implementation- PPE-98280', function () {
         otfTab.DeleteOutputVersion();
     });
 
-    it('Should Verify the folder placement of created Output Version object', function(){
+    it('Should Verify the folder placement of created Output Version object - PPE-107804', function(){
         browser.pause(2000);
         otfOutputVersion.MovetoframeParent();
         repositoryBrowserTab.openFolder('webmd/professional_assets/medscape/media/output_version');
@@ -65,7 +65,7 @@ describe('OTF Profoutput-Implementation- PPE-98280', function () {
         otfOutputVersion.MovetoframeParent();
         otfOutputVersion.OutputVersionProperties();
     });
-    it.skip('Should Verify properties of OutPut version object through OTF widget', function () {
+    it.skip('Should Verify properties of OutPut version object through OTF widget - PPE-108803', function () {
         otfTab.selectOTFTab();
         otfTab.selectExternalWidget();
         otfTab.CreateOutputVersionIMP(global.d2ProfDataSettings.otfData.OutputTypeaudio);
--- test/d2/prof/fe/jira/profPhase3Sprint1/ppe-98280-PT.js
+++ test/d2/prof/fe/jira/profPhase3Sprint1/ppe-98280-PT.js
@@ -57,7 +57,7 @@ describe('OTF Profoutput-Implementation- PPE-98280', function () {
         otfTab.DeleteOutputVersion();
     });
 
-    it('Should Verify the folder placement of created Output Version object', function(){
+    it('Should Verify the folder placement of created Output Version object - PPE-107804', function(){
         browser.pause(2000);
         otfOutputVersion.MovetoframeParent();
         repositoryBrowserTab.openFolder('webmd/professional_assets/medscape/media/output_version');
@@ -66,7 +66,7 @@ describe('OTF Profoutput-Implementation- PPE-98280', function () {
         otfOutputVersion.OutputVersionProperties();
     });
 
-    it.skip('Should Verify properties of OutPut version object through OTF widget', function () {
+    it.skip('Should Verify properties of OutPut version object through OTF widget - PPE-108803', function () {
         otfTab.selectOTFTab();
         otfTab.selectExternalWidget();
         otfTab.CreateOutputVersionIMP(global.d2ProfDataSettings.otfData.OutputTypeaudio);
