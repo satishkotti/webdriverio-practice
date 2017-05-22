4439b41611f977605d49ba8cce2e68bb7905ec23
unknown <ddutta@E1395F1L.lsz.ads.valuelabs.net>
2017-05-22 16:29:03 +0530

    Updated methd

--- Test/Baseline_tests/common/functions/FE_Smoke_SeeAllpages_Actions.js
+++ Test/Baseline_tests/common/functions/FE_Smoke_SeeAllpages_Actions.js
@@ -11,26 +11,12 @@ var sap_Actions = require('./../../common/functions/FE_Smoke_SeeAllpages_Actions
 
 module.exports =
     {
-        Get_saptitle: function()
-        {
-            var sap_title = browser.getTitle();
-            console.log(sap_title);
-        },
-        
- SeeAll_grid_working: function(menuitem)
- {
-            browser.leftClick(menuitem);
-            browser.pause(1000);
-        },
-        page_header: function(page_header)
-        {
-            var ph_visible=browser.isVisible(page_header);
-            assert.equal(ph_visible,true);
-        },
-        see_all_grid: function(see_all_grid)
-        {
-            var sag_visible=browser.isVisible(see_all_grid);
-            assert.equal(sag_visible,true);
+
+            see_all_grid: function (see_all_grid) {
+            var ph_visible = browser.isVisible(page_header);
+            var sag_visible = browser.isVisible(see_all_grid);
+            assert.equal(ph_visible, true);
+            assert.equal(sag_visible, true);
         },
 
-}
+    }
