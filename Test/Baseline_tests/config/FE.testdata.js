cb1f59d1dcc960b979f869ddf95b872ed5187a9b
unknown <ddutta@E1395F1L.lsz.ads.valuelabs.net>
2017-05-24 13:49:55 +0530

    Updated config files

--- Test/Baseline_tests/config/FE.testdata.js
+++ Test/Baseline_tests/config/FE.testdata.js
@@ -2,17 +2,17 @@ module.exports=
 {
   preview:
   {
-    //environment:"http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/art-2",
-    //-- for Articles-----
-        Article_url: 'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/art-1',
-    //-----for ISI
-        ISI_url:'http://www.preview.webmd.com/zz-test-sp/isi',
-     //For slideshow---
-        SS_url: 'http://www.preview.webmd.com/diet/ss/slideshow-healthy-eating-resolutions',   
-    //TOC--
-      TOC_url:'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/default.htm',
-    //See All
-      See_All_url:'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/see-all-page#see-all-non-spon',     
+    // //environment:"http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/art-2",
+    // //-- for Articles-----
+    //     Article_url: 'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/art-1',
+    // //-----for ISI
+    //     ISI_url:'http://www.preview.webmd.com/zz-test-sp/isi',
+    //  //For slideshow---
+    //     SS_url: 'http://www.preview.webmd.com/diet/ss/slideshow-healthy-eating-resolutions',   
+    // //TOC--
+    //   TOC_url:'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/default.htm',
+    // //See All
+    //   See_All_url:'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/see-all-page#see-all-non-spon',     
  },
  staging:
  {
@@ -20,13 +20,13 @@ module.exports=
     //-- for Articles-----
         Article_url: 'http://www.staging.webmd.com/zztest/revenue-products/fed2/seg1/art-1',
    //-----for ISI
-        ISI_url:'http://www.staging.webmd.com/zz-test-sp/isi',
-     //For slideshow---
-        SS_url: 'http://www.staging.webmd.com/diet/ss/slideshow-healthy-eating-resolutions',    
-    //TOC--
-        TOC_url: 'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/default.htm',
-    // See All
-        See_all_url:'http://www.staging.webmd.com/zztest/revenue-products/fed2/seg1/see-all-page#see-all-non-spon',
+    //     ISI_url:'http://www.staging.webmd.com/zz-test-sp/isi',
+    //  //For slideshow---
+    //     SS_url: 'http://www.staging.webmd.com/diet/ss/slideshow-healthy-eating-resolutions',    
+    // //TOC--
+    //     TOC_url: 'http://www.preview.webmd.com/zztest/revenue-products/fed2/seg1/default.htm',
+    // // See All
+    //     See_all_url:'http://www.staging.webmd.com/zztest/revenue-products/fed2/seg1/see-all-page#see-all-non-spon',
 
  }
  }
--- Test/Baseline_tests/wdio.conf.js
+++ Test/Baseline_tests/wdio.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     //
     specs: [
       // './test/profile&revenue/prdemo/fe/jira/PPE-101748/PPE-103377.js'
-    './test/profile&revenue/prdemo/fe/jira/smoke/FE_Smoke_Article.js'
+    './smoke/FE_Smoke_Article.js'
      //'./test/profile&revenue/prdemo/fe/jira/smoke/FE_Smoke_SS.js'
      // './test/profile&revenue/prdemo/fe/jira/smoke/FE_Smoke_ISI_Module.js'
      //  './test/profile&revenue/prdemo/fe/jira/Archived/FE_Smoke_TOC_Module.js'
