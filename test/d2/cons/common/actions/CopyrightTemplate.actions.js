c15719274716917ed9b11f92fce706a500f3cc13
LSZ\gaddam.thirupathi <TGaddam@webmd.net>
2017-05-25 18:56:46 +0530

    +++PPE -61754 Copyright Template UK  Base line scripts genrated.

---
+++ test/d2/cons/common/actions/CopyrightTemplate.actions.js
@@ -0,0 +1,36 @@
+var CopyrightUI = require('./../ui/CopyrightTemplate');
+var contentTabUI = require('./../ui/contentTab');
+
+var CopyrightObj = {
+   CopyrightStatementText: function(Textvalue){
+        contentTabUI.switchToExternalWidgetFrame();
+        CopyrightUI.CopyrightStatementSetValue(Textvalue);
+    },
+  setRequiredPropertiesCpyRights: function(Titlaval,Cpyrghthldr,Lglrvwrset){
+        propertiesTabUI.propertiesTabSelect();
+        propertiesTabUI.edit();
+        CopyrightUI.TitleSet(Titlaval);
+        CopyrightUI.CopyrightHolderSet(Cpyrghthldr);
+        CopyrightUI.LegalReviewerSet(Lglrvwrset);
+        CopyrightUI.CopyrightStartDateSet();
+        CopyrightUI.CopyrightEndDateSet();
+        CopyrightUI.LegalReviewDateSet();
+        CopyrightUI.publishingTabSelect();
+        CopyrightUI.RetentionDateSet();
+        propertiesTabUI.save();
+    },
+
+     setRequiredPropertiesOthr: function(subject,languge_code){
+        propertiesTabUI.propertiesTabSelect();
+        propertiesTabUI.edit();
+        CopyrightUI.otherTabSelect();
+        CopyrightUI.TitleSet(subject);
+        CopyrightUI.CopyrightHolderSet(languge_code);
+        propertiesTabUI.save();
+    }
+
+    
+    
+}
+
+module.exports = CopyrightObj;
---
+++ test/d2/cons/fe/jira/profport062017/PPE-61754.js
@@ -0,0 +1,90 @@
+var Promise = require('bluebird');
+var JSONPath = require('JSONPath');
+var parseXml = require('./../../../common/components/parseXml');
+var functions = require('./../../../common/functions/functions');
+var login = require('./../../../common/actions/login.actions');
+var repositoryBrowserTab = require('./../../../common/actions/repositoryBrowserTab.actions');
+var workspaceMenu = require('./../../../common/actions/workspace.menu.actions');
+var documentListTab = require('./../../../common/actions/documentListTab.actions');
+var contentTab = require('./../../../common/actions/contentTab.actions');
+var ckEditorMenu = require('./../../../common/actions/ckEditor.actions');
+var CopyrightTemplate = require('./../../../common/actions/CopyrightTemplate.actions');
+var propertiesTab = require('./../../../common/actions/propertiesTab.actions');
+var randomstring = require("randomstring");
+
+describe('Regression', function () {
+
+    var chronicleId;
+    var AssetTitle;
+    var AssetName;
+    var cidName;
+     var objName;
+     var CopyrightStatementText="";
+    before(function () {
+        login.loginWithNewWindow({
+            url: functions.getEnvTestUrl(),
+            username: functions.getQAPublicationUser().username,
+            password: functions.getQAPublicationUser().password
+        });
+        repositoryBrowserTab.openFolder(global.d2ConDataSettings.inputData.UKCopyrightTFolderPath);
+        AssetTitle = global.d2ConDataSettings.inputData.ArticleObjectName + randomstring.generate(2);
+        AssetName = global.d2ConDataSettings.inputData.ArticleDescription + randomstring.generate(2);
+        workspaceMenu.createContent(
+            global.d2ConDataSettings.inputData.ArticleProfileName,
+            global.d2ConDataSettings.inputData.ArticleTemplate,
+            AssetTitle,
+            AssetName);
+    });
+
+    
+     it('Verify the checkout , cancel and checkin operation', function () {
+        documentListTab.selectAsset(AssetTitle);
+        contentTab.checkOut();
+        contentTab.cancel();
+        contentTab.checkOut();
+        contentTab.checkIn();
+    });
+
+    it('Verify the Mandatory fields, promote, demote and power promote', function () {
+        documentListTab.selectAsset(AssetTitle);
+        cidName = propertiesTab.getChronicleIdAndName();
+        objName = cidName.objectName;
+        chronicleId = cidName.chronicleId;
+        CopyrightTemplate.setRequiredPropertiesCpyRights(objName, 'WebMD', 'QA_TestPerson1234');
+        CopyrightTemplate.setRequiredPropertiesOthers('Testsubjct', 'en_US');
+        contentTab.checkOut();
+        CopyrightTemplate.CopyrightStatementText(CopyrightStatementText);
+        browser.frameParent();
+        contentTab.checkIn();
+        documentListTab.selectAsset(AssetTitle);
+        documentListTab.powerPromoteAsset(AssetTitle);
+        documentListTab.publishAssetToStaging(AssetTitle);
+        browser.pause(5000);
+
+           browser.call(function () {
+            return Promise.resolve(
+                parseXml.getXmlFromUrl(functions.getAtsScsFileUrl() + chronicleId, null).then(function (result) {
+                    var style = 'float:' + global.d2ConDataSettings.inputData.ShareableAlign.toLowerCase() + ';';
+                    var Asset = JSONPath({
+                        json: result,
+                        path: "$..metadata_section",
+                        resultType: 'all'
+                    });
+
+                     var Content = JSONPath({
+                        json: result,
+                        path: "$..content_section",
+                        resultType: 'all'
+                    });
+
+                    expect(Asset[0].parent.metadata_section.i_chronicle_id).to.equal(chronicleId);
+                    expect(Content[0].parent.content_section.wbmd_copyright.wbmd_copyright_statement).to.equal(CopyrightStatementText);
+                    
+
+                      }));
+        });
+
+
+    });
+    
+});
