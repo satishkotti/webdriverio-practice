fbc1c310c64138bf91eaef253d692c7bbc65205b
DDutta <ddutta@webmd.net>
2017-05-31 22:00:14 +0530

    PPE-112216 : Updated cold and Flu test data

--- Test/jira/config/Jira_testdata.js
+++ Test/jira/config/Jira_testdata.js
@@ -2,7 +2,7 @@ module.exports= {
   
  staging:{
    ICM_url:"http://www.staging.webmd.com/zztest/test/responsive-icm?id=091e9c5e81572b7f",
-   socialicons_URL:"http://www.staging.webmd.com/zztest/revenue-products/fed2/seg1/art-2"
-     
+   socialicons_URL:"http://www.staging.webmd.com/zztest/revenue-products/fed2/seg1/art-2",
+     cold_and_flu_URL:"http://symptoms.staging.webmd.com/cold-flu-map-tool/default.htm"
  }
  }
