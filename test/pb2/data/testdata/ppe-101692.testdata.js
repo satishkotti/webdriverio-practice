b4b29147d1a8e746a181b59e993ccb7d52d1956b
Sandeep Putla <sputla@webmd.net>
2017-06-14 17:15:41 +0530

    PPE-108735: Removed unnecessary files

--- test/pb2/data/testdata/ppe-101692.testdata.js
+++
@@ -1,7 +0,0 @@
-var testEnv = global.testEnv;
-module.exports = {
-    SetTestEnv: function(testEnvironemnt){
-        testEnv =  this.testEnvironemnt;
-    },
-    from: "http://www." + testEnv + ".webmd.com/food-recipes/guide/health-cooking*",
-}
