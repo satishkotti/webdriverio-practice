6df39a4032c83a855878b86f8cf592179c425631
sushma surekha <SKodavali@webmd.net>
2017-06-22 18:52:07 +0530

    PPE-116300 : modified code for FE_Smoke_Slideshow module, changed locators of the elements for this module

--- Test/Baseline/Desktop/common/functions/FE_Smoke_SS_Action.js
+++ Test/Baseline/Desktop/common/functions/FE_Smoke_SS_Action.js
@@ -1,20 +1,15 @@
 var webdriverio = require('webdriverio');
 var should = require('should');
-var path = require('path');
-var rootPath = path.normalize(__dirname)
 var argv = require("yargs").argv;
 var assert = require("assert");
-console.log(__dirname);
 var ssElements = require('../elements/FE_Smoke_SS_Elements');
-
 module.exports = {
-    Imageandtitle_visibility: function() {
+    Imageandtitle_visibility: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
             browser.leftClick(ssElements.secondary_next.selector);
-            var img = browser.isVisible('//div [@class="webmd-tool-slideshow"]//div [@class="owl-stage"]/div[' + i + ']/div[@class="slide"]//div [@class="image"]/img');
-            console.log(img[i]);
-            var titlevisible = browser.isVisible('//div [@class="webmd-tool-slideshow"]//div [@class="owl-stage"]/div[' + i + ']/div[@class="slide"]/div [@class="caption"]/h2');
+            var img = browser.isVisible('//div [@class="owl-stage-outer owl-height"]//div [@class="owl-stage"]/div["+i+"]/div/div[1]/img');
+            var titlevisible = browser.isVisible('//div [@class="owl-stage"]/div["+i+"]/div/div[2]/h2');
             assert.equal(img[i], true);
             assert.equal(titlevisible[i], true);
             browser.pause(1000);
@@ -24,22 +19,21 @@ module.exports = {
             browser.leftClick(ssElements.secondary_prev.selector);
             browser.pause(1000);
         }
-
     },
     //Verify that Disclaimer text is visible on the vrey first slide
-    Disclaimer_visibility: function() {
+    Disclaimer_visibility: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
             if (i == 1) {
-                var sourcevisible = browser.isVisible(ssElements.tools_disc.selector);
+                var sourcerightvisible = browser.isVisible(ssElements.tools_disc.selector);
                 assert.equal(sourcerightvisible, true);
             } else {
-                var sourcevisible = browser.isVisible(ssElements.tools_disc.selector);
+                var sourcerightvisible = browser.isVisible(ssElements.tools_disc.selector);
                 assert.equal(sourcerightvisible, false);
             }
             browser.leftClick("//div[@class='controls secondary']/a[@class='next']/i");
             var Disc_visible = browser.isVisible(ssElements.tools_disc.selector);
-            assert.equal(Disc_visible, true);
+            assert.equal(Disc_visible, false);
             browser.pause(1000);
         }
         for (var i = elements;
@@ -48,8 +42,7 @@ module.exports = {
             browser.pause(1000);
         }
     },
-
-    Background_color_secondary: function() {
+    Background_color_secondary: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
             if (i == 1) {
@@ -75,9 +68,8 @@ module.exports = {
             browser.leftClick('//div [@id="dyn-ss"]//div [@class="controls primary"]//a [@class="prev"]/i');
             browser.pause(1000);
         }
-
     },
-    Source_working: function() {
+    Source_working: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
             browser.leftClick("//div[@class='controls secondary']/a[@class='next']/i");
@@ -89,7 +81,7 @@ module.exports = {
             browser.pause(1000);
         }
     },
-    Background_color_primary: function() {
+    Background_color_primary: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         console.log(elements);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
@@ -99,7 +91,6 @@ module.exports = {
                 assert.equal(backgroundcolorp.parsed.hex, '#dbdad9');
                 var visible = browser.isVisible('//div [@id="dyn-ss"]//div [@class="controls primary"]//a [@class="prev"]/i');
                 assert.equal(visible, false);
-                console.log(visible);
             }
             browser.leftClick('//div [@class="controls primary"]//a [@class="next"]/i');
             var backgroundcolorn = browser.getCssProperty('//div [@class="controls primary"]//a [@class="next"]/i', 'background');
@@ -113,11 +104,10 @@ module.exports = {
             browser.pause(1000);
         }
     },
-    Check_primary_next_previous_button: function() {
+    Check_primary_next_previous_button: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
             var currentslide = browser.getText(ssElements.secondary_currslide.selector);
-            //console.log(currentslide);
             browser.leftClick('//div [@class="controls primary"]//a [@class="next"]/i');
             browser.pause(1000);
             currentslide++;
@@ -137,10 +127,20 @@ module.exports = {
             if (currentslide == curr_slide) {
                 console.log("previous button is working");
             }
-
         }
     },
-    Check_secondary_next_previous_button: function() {
+    /* Searching for Breadcrumb test to be visible */
+    Search: function (Icons, scroll_value) {
+        var breadcrumb_text = ssElements.Breadcrumb.getText();
+        var flag_visible = browser.isVisible(Icons);
+        browser.pause(4000);
+        var actions = {
+            breadcrumb_text: breadcrumb_text,
+            flag_visible: flag_visible,
+        }
+        return actions;
+    },
+    Check_secondary_next_previous_button: function () {
         var elements = browser.getText(ssElements.slides_Count.selector);
         //console.log(elements);
         for (var i = 1; i < parseInt(elements) - 1; i++) {
@@ -152,7 +152,6 @@ module.exports = {
             if ((currentslide) == (curr_slide)) {
                 console.log("next button is working");
             }
-
         }
         for (var i = elements;
             ((i <= elements) && (i != 2)); i--) {
@@ -165,24 +164,20 @@ module.exports = {
             if (currentslide == curr_slide) {
                 console.log("previous button is working");
             }
-
         }
-
     },
-    Get_primary_currslide: function(i) {
+    Get_primary_currslide: function (i) {
         var curr_slide = "//div[1]/div/div/div[" + i + "]/div[2]/div/span[1]";
         return curr_slide;
     },
-
-    Check_image_validation: function() {
+    Check_image_validation: function () {
         var primary_cur_slide, secondary_curr_slide, slide_num;
-        var slides_Count = browser.getText(ssElements.slides_Count.selector); //r
+        var slides_Count = browser.getText(ssElements.slides_Count.selector); 
         for (var i = 1; i <= parseInt(slides_Count) - 1; i++) {
             secondary_curr_slide = browser.getText(ssElements.secondary_currslide.selector); //get the bottom current slide num
-            //click on the image on SS
             browser.pause(2000);
             browser.scroll(ssElements.image.selector);
-            browser.click(ssElements.image.selector); //r
+            browser.click(ssElements.image.selector); 
             if (get_primary_currslide(secondary_curr_slide))
                 secondary_curr_slide++; //increment the curr slide num   
             slide_num = browser.getText(ssElements.secondary_currslide.selector); //get the slide num of current slide
@@ -190,12 +185,8 @@ module.exports = {
             // top current slide number  and bottom current slide number shoulb be equal to slide num
             if ((primary_cur_slide === slide_num) && (secondary_curr_slide === slide_num))
                 return true;
-
-
             else
                 return false;
-
         }
     }
-
 }
--- wdio.conf.js
+++ wdio.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     specs: [
       // './test/profile&revenue/prdemo/fe/jira/PPE-101748/PPE-103377.js'
     //'./Test//smoke/FE_Smoke_Article.test.js'
-    './Test/Baseline/Desktop/smoke/FE_Smoke_Article.test.js'
+    './Test/Baseline/Desktop/smoke/FE_Smoke_SS.test.js'
      //'./test/profile&revenue/prdemo/fe/jira/smoke/FE_Smoke_SS.js'
      // './test/profile&revenue/prdemo/fe/jira/smoke/FE_Smoke_ISI_Module.js'
      //  './test/profile&revenue/prdemo/fe/jira/Archived/FE_Smoke_TOC_Module.js'
