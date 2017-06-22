08b58366181533162db5be5fb318ff63aaaaee67
Bhaskar Sistla <bsistla@webmd.net>
2017-06-23 03:30:37 +0530

    PPE-105234 : Added test cases. (TCs: PPE-117284, PPE-117285, PPE-117286, PPE-117287, PPE-117288, PPE-117289, PPE-117341, PPE-117432)

--- test/pb2/fe/jira/release29/ppe-105234/PPE-105234.js
+++ test/pb2/fe/jira/release29/ppe-105234/PPE-105234.js
@@ -15,6 +15,12 @@ describe('PPE-105234: Verify Bulk Import functionality', function() {
     it('Go to Redirect Tool Page', function() {
         redirectActions.GoToRedirectToolPage();
     }); 
+
+    it("Verify the application accepts only xls and xlsx files", function() {
+        redirectActions.BulkImport();
+        redirectActions.UploadRedirects("test\\pb2\\fe\\jira\\release29\\ppe-105234\\test.txt");
+        expect(browser.element("//section/ul/li").isVisible()).to.be.false;
+    });
         /**
          * This test covers the following tests.
          * Verify user is not able to create redirects with loop redirects
@@ -27,7 +33,8 @@ describe('PPE-105234: Verify Bulk Import functionality', function() {
          */
 
     it("Verify import form validation", function() {
-       test.ImportRedirects("test\\pb2\\fe\\jira\\release29\\ppe-105234\\dev03_bulkimport_invalid.xlsx");
+       redirectActions.UploadRedirects("test\\pb2\\fe\\jira\\release29\\ppe-105234\\dev03_bulkimport_invalid.xlsx");
+       browser.pause(3000);
        expect(browser.element("//button[contains(text(),'Create')]").isEnabled()).to.be.false;
        expect(browser.element("//section/ul/li[4]/div/div[3]/span/span[contains(text(),'Multi-hop redirects are not allowed')]").isVisible()).to.be.true;
        expect(browser.element("//section/ul/li[5]/div/div[3]/span/span[contains(text(),'Multi-hop redirects are not allowed')]").isVisible()).to.be.true;
@@ -61,7 +68,7 @@ describe('PPE-105234: Verify Bulk Import functionality', function() {
        //Submit the form
        browser.element("//button[contains(text(),'Create')]").click();
        expect(browser.element("//button[contains(text(),'Create')]").isEnabled()).to.be.false;
-       browser.pause(120000);
+       browser.pause(50000);
        browser.element("//div[@class='modal-content']/div[@class='pb-overlay-content ng-scope']").waitForVisible();
        expect(browser.element("//div[@class='modal-content']/div[@class='pb-overlay-content ng-scope']/div").getText()).to.equal("Redirect Import Failure");
        browser.element("#modal-ok").click();
@@ -69,138 +76,25 @@ describe('PPE-105234: Verify Bulk Import functionality', function() {
     });
     
     it("Verify user is able to submit the form after resolving API errors", function() {
-       //section/ul/li[8]/div/div[1]/label/input"
-       //section/ul/li[8]/div/div[2]/label/input"
+        var fromUrlList = [];
+        var toUrlList=[];
         for (i=1;i<10;i++){
-            console.log(i);
-            var fromUrl = browser.element("//section/ul/li[" + i + "]/div/div[1]/label/input").getValue();
-            console.log(fromUrl);
-            browser.element("//section/ul/li[" + i +"]/div/div/label/input").setValue(fromUrl+"/testing_purpose");
-            var toUrl = browser.element("//section/ul/li[" + i + "]/div/div[2]/label/input").getValue();
-            console.log(toUrl);
-            browser.element("//section/ul/li[" + i +"]/div/div/label/input").setValue(toUrl+"/test_purpose");
+            var fromUrl = browser.element("//section/ul/li[" + i + "]/div/div[1]/label/input").getValue() + "/qaactivity031823062017";
+            fromUrlList.push(fromUrl);
+            browser.element("//section/ul/li[" + i +"]/div/div[1]/label/input").setValue(fromUrl);
+
+            var toUrl = browser.element("//section/ul/li[" + i + "]/div/div[2]/label/input").getValue() + "/qaactivity031823062017";
+            toUrlList.push(toUrl);
+            browser.element("//section/ul/li[" + i +"]/div/div[2]/label/input").setValue(toUrl);
         }
         browser.element("//button[contains(text(),'Create')]").click();
         expect(browser.element("//button[contains(text(),'Create')]").isEnabled()).to.be.false;
-        browser.element("//div[@class='modal-content']/div[@class='pb-overlay-content ng-scope']").waitForVisible();
+        browser.waitForVisible("section.pb-notification-container.success");  
+        if(browser.element("=Search Criteria").isVisible())
+            browser.element("=Search Criteria").click();
+        redirectActions.Search({'from':fromUrlList[0], 'to':null});
+        browser.pause(3000);
+        expect(browser.getText("//tbody[@role='rowgroup']//td[3]/a")).to.equal(fromUrlList[0]);
+        expect(browser.getText("//tbody[@role='rowgroup']//td[6]/a")).to.equal(toUrlList[0]);
     });
-
-
-    /*
-    it("Verify user is not able to create redirects with bulk import", function() {
-       test.ImportRedirects("test\\pb2\\fe\\jira\\release29\\ppe-105234\\dev03_bulkimport_valid.xlsx");
-    });
-
-    it("User should be able to delete single record from the result", function() {
-         browser.refresh();
-        //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
-        browser.pause(5000);
-        //Check first chechbox if available
-        if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]')){
-            var chronID=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]'); 
-            //checkbox.checked=true
-            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[1]/input').click();        
-            if(browser.isVisible("/html/body/main/section[1]/button")){
-                // click delete button
-                browser.element("/html/body/main/section[1]/button").leftClick();
-                //confirm delete
-                browser.element('//*[@id="modal-ok"]').leftClick(); 
-                browser.waitForVisible("section.pb-notification-container.success");  
-
-                browser.refresh();
-                 redirectActions.SearchFromUrl(chronID);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result, false); 
-
-            }
-        }        
-       
-        
-    });
-
-    it("User should be able to delete multiple records from the result", function() {
-         browser.refresh();
-        //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
-        browser.pause(5000);
-        //Check first chechbox if available
-        if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[4]')){
-            var chronID1=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]'); 
-            var chronID2=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[4]'); 
-            //checkbox.checked=true
-            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[1]/input').click();  
-            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[1]/input').click();       
-            if(browser.isVisible("/html/body/main/section[1]/button")){
-                // click delete button
-                browser.element("/html/body/main/section[1]/button").leftClick();
-                //confirm delete
-                browser.element('//*[@id="modal-ok"]').leftClick();      
-                 browser.waitForVisible("section.pb-notification-container.success");  
-
-                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID1);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result1 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result1, false); 
-
-                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID2);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result2 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result2, false);  
-
-            }
-        }        
-        
-    });
-
-      it("User should be able to select and delete all data available in page", function() {
-        browser.refresh();
-        //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
-        browser.pause(5000);
-        //Check first chechbox if available
-        if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]')){
-             var chronID=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]');  
-            //checkbox.checked=true
-            browser.element('//input[@class="pb-redirect-check-all"]').click();
-            browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');
-            var totalRecords = browser.elements("input:checked[type='checkbox']").value.length-1;
-            //get Total record  
-            var paggingTextBefore=browser.getText('//*[@id="redirectListGrid"]/div[4]/span'); 
-            var countBefore=paggingTextBefore.substring(paggingTextBefore.indexOf("of")+3,paggingTextBefore.indexOf("redirects")-1);
-            // click delete button
-            browser.element("/html/body/main/section[1]/button").leftClick();
-            //confirm delete
-            browser.element('//*[@id="modal-ok"]').leftClick();       
-            var result = (browser.isVisible('/html/body/div[5]'));
-            var countAfter=0;
-            if(result){
-                browser.refresh();
-                redirectActions.SearchFromUrl(correctEnvUrl);                 
-                browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');
-                var paggingTextAfter=browser.getText('//*[@id="redirectListGrid"]/div[4]/span'); 
-                countAfter=paggingTextAfter.substring(paggingTextAfter.indexOf("of")+3,paggingTextAfter.indexOf("redirects")-1);
-                assert.equal((countAfter=countBefore-totalRecords), true);
-
-                browser.refresh();
-                 redirectActions.SearchFromUrl(chronID);
-                 browser.pause(5000);
-                 //verify deletion completed  
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');     
-                 var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result, false);
-            }
-        }        
-        
-    });
-    */
 });
