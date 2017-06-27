4bdf72ee257d1761c613b8cee0c7443105496b7c
Bhaskar Sistla <bsistla@webmd.net>
2017-06-27 20:02:25 +0530

    PPE-105235: Refactoring tests for PPE-101694 changing the usage of navigating to redirect tool. (TCs: PPE-114515)

--- test/pb2/fe/jira/release29/PPE-101694.js
+++ test/pb2/fe/jira/release29/PPE-101694.js
@@ -13,15 +13,14 @@ describe('PPE-101694: Redirect Search Results', function() {
     });
 
     it('Go to Redirect Tool Page', function() {
-        redirectActions.GoToRedirectToolPage();
+        test.NavigateToRedirectToolPage();
     }); 
 
     it("User should not be able to delete from the result if not confirmed", function() {
-       
         //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
-        browser.pause(5000);     
-        
+        redirectActions.Search({'from':correctEnvUrl, 'to':null});
+        browser.pause(5000);
+
         //Check first chechbox if available
         if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]')){
             //get Chronical ID  
@@ -37,21 +36,20 @@ describe('PPE-101694: Redirect Search Results', function() {
                 expect(result).to.be.false
 
                  browser.refresh();
-                 redirectActions.SearchFromUrl(chronID);
+                 redirectActions.Search({'from':chronID, 'to': null});
                  browser.pause(5000);
                  //verify deletion completed  
                  browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');     
                  var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
                  assert.equal(result, true);
             }
-        }        
-        
+        }
     });
 
     it("User should be able to delete single record from the result", function() {
          browser.refresh();
         //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
+        redirectActions.Search({'from':correctEnvUrl, 'to':''});
         browser.pause(5000);
         //Check first chechbox if available
         if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]')){
@@ -66,63 +64,61 @@ describe('PPE-101694: Redirect Search Results', function() {
                 browser.waitForVisible("section.pb-notification-container.success");  
 
                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result, false); 
-
+                redirectActions.Search({'from':chronID, 'to':''});
+                browser.pause(5000);
+                //verify deletion completed   
+                browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
+                var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
+                assert.equal(result, false); 
             }
-        }        
-       
-        
+        }
     });
 
-    it("User should be able to delete multiple records from the result", function() {
+    it.only("User should be able to delete multiple records from the result", function() {
          browser.refresh();
+         test.NavigateToRedirectToolPage();
         //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
+        redirectActions.Search({'from':correctEnvUrl, 'to':''});
         browser.pause(5000);
         //Check first chechbox if available
         if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[4]')){
-            var chronID1=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]'); 
-            var chronID2=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[4]'); 
+            var chronID1=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[3]');
+            console.log("deleting1", chronID1);
+            var chronID2=browser.getText('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[3]');
+            console.log("deleting2", chronID2);
             //checkbox.checked=true
-            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[1]/input').click();  
-            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[1]/input').click();       
+            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[1]/input').click();
+            browser.element('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[2]/td[1]/input').click();
             if(browser.isVisible("/html/body/main/section[1]/button")){
                 // click delete button
                 browser.element("/html/body/main/section[1]/button").leftClick();
                 //confirm delete
-                browser.element('//*[@id="modal-ok"]').leftClick();      
-                 browser.waitForVisible("section.pb-notification-container.success");  
+                browser.element('//*[@id="modal-ok"]').leftClick();
+                browser.waitForVisible("section.pb-notification-container.success");  
 
-                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID1);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result1 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result1, false); 
+                browser.refresh();
+                redirectActions.Search({'from':chronID1, 'to':''});
+                browser.pause(5000);
+                //verify deletion completed   
+                browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');
+                var result1 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
+                assert.equal(result1, false); 
 
-                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID2);
-                 browser.pause(5000);
-                 //verify deletion completed   
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
-                 var result2 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result2, false);  
-
+                browser.refresh();
+                redirectActions.Search({'from':chronID2, 'to':''});
+                browser.pause(5000);
+                //verify deletion completed   
+                browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');    
+                var result2 = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
+                assert.equal(result2, false);  
             }
-        }        
-        
+        }
     });
 
       it("User should be able to select and delete all data available in page", function() {
         browser.refresh();
         //from
-        redirectActions.SearchFromUrl(correctEnvUrl);
+        redirectActions.Search({'from':correctEnvUrl, 'to':''});
         browser.pause(5000);
         //Check first chechbox if available
         if(browser.isVisible('//*[@id="redirectListGrid"]/div[3]/table/tbody/tr[1]/td[4]')){
@@ -137,26 +133,25 @@ describe('PPE-101694: Redirect Search Results', function() {
             // click delete button
             browser.element("/html/body/main/section[1]/button").leftClick();
             //confirm delete
-            browser.element('//*[@id="modal-ok"]').leftClick();       
+            browser.element('//*[@id="modal-ok"]').leftClick();
             var result = (browser.isVisible('/html/body/div[5]'));
             var countAfter=0;
             if(result){
                 browser.refresh();
-                redirectActions.SearchFromUrl(correctEnvUrl);                 
+                redirectActions.Search({'from':correctEnvUrl,'to':''});
                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');
                 var paggingTextAfter=browser.getText('//*[@id="redirectListGrid"]/div[4]/span'); 
                 countAfter=paggingTextAfter.substring(paggingTextAfter.indexOf("of")+3,paggingTextAfter.indexOf("redirects")-1);
                 assert.equal((countAfter=countBefore-totalRecords), true);
 
                 browser.refresh();
-                 redirectActions.SearchFromUrl(chronID);
-                 browser.pause(5000);
-                 //verify deletion completed  
-                 browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');     
-                 var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
-                 assert.equal(result, false);
+                redirectActions.Search({'from':chronID,'to':''});
+                browser.pause(5000);
+                //verify deletion completed  
+                browser.waitForVisible('//*[@id="redirectListGrid"]/div[4]/span');     
+                var result = (browser.isVisible('tbody > tr:first-child > td:first-child > input.pb-checkbox'));
+                assert.equal(result, false);
             }
-        }        
-        
+        }
     });
 });
