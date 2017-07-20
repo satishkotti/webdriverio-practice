0f9548bd8614ab581fa2931805abf235f347e7f1
Sandeep Putla <sputla@webmd.net>
2017-07-20 18:24:28 +0530

    PPE-94120: Added complete test code for PPE-120822, PPE-120823, PPE-120824, PPE-120825, PPE-120826, PPE-120827, PPE-120828, PPE-120830, PPE-120831, PPE-120832 Added new tests: PPE-121625, PPE-121626, PPE-121627, PPE-121628

---
+++ test/pb2/data/testdata/ppe-94120.testdata.js
@@ -0,0 +1,99 @@
+module.exports = {
+    ppe_120822:
+    {
+        xsl_name: '',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120823:
+    {
+
+    },
+
+    ppe_120824:
+    {
+        xsl_name: '',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120825:
+    {
+
+    },
+
+    ppe_120826:
+    {
+        xsl_name: '',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120827:
+    {
+        xsl_name: '',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120828:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120830:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120831:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_120832:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+        expected_version: '',
+        expected_stage: ''
+    },
+
+    ppe_121625:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+    },
+
+    ppe_121626:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+    },
+
+    ppe_121627:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+    },
+
+    ppe_121628:
+    {
+        xsl_name:'',
+        xsl_chronID: '',
+    },
+}
