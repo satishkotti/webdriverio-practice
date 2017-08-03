114acfb6d9fe6fe2986cc42acedb84acba7e8a3c
Bhaskar Sistla <bsistla@webmd.net>
2017-08-03 19:36:21 +0530

    PPE-117815. Added test case.(TestCases: PPE-124263)

---
+++ test/pb2/fe/jira/release2_10/ppe-117815/PPE-117815.js
@@ -0,0 +1,62 @@
+var assert = require('assert');
+var test = require('./../../../../common/functions/functions');
+var iwc = require('./../../../../common/actions/iwc.actions');
+var props = require('./../../../../common/actions/assetprops.actions');
+var pageTestData = require('./../../../../data/page.assets');
+var act = require('./../../../../common/actions/assetactions.actions');
+
+var testEnv = global.testEnv;
+if (testEnv === 'qa02')
+    testEnv = 'perf';
+
+function checkATSStatus(chronID){
+    test.NavigatetoATSStatusCheckerPageOf(chronID, 'live');
+    browser.pause(2000);
+    test.WaitForATSFile();
+}
+
+function handleRuntimeValidation(expected_protocol){
+    var handles = browser.windowHandles();
+    browser.switchTab(handles.value[1]);
+    var actual_protocol = browser.getUrl().split(':')[0];
+    expect(actual_protocol === expected_protocol).to.be.true;
+    browser.close();
+    browser.switchTab(handles.value[0]);
+}
+
+describe('PPE-105015: Verify the file naming convention for PB page/template CSS', function() {
+    before(() => {
+        //Launch App
+        test.LaunchAppAndLogin();
+    });
+
+    it("Verify the SSL configuration is set to true by default", function() {
+        var assetDetails = pageTestData.normalStandalonePage;
+        test.EnterIWC('Create', 'Templates & Pages');
+        test.TraverseSS('Level 0/zzTest/QA and Dev');
+        iwc.AddToNode("Page"); 
+        var sslconfig = act.GetSSLConfig();
+        expect(sslconfig).to.be.true;
+        assetChronID = props.PopulatePageProps(assetDetails);
+        test.SaveOrPublishTheAsset('publish to live', 'Test');
+        checkATSStatus(assetChronID);
+        test.ClickButtonInATSPage("Redirect to URL");
+        handleRuntimeValidation('https');
+    });
+
+    it.only("Verify the SSL configuration for existing pages remain unchanged", function() {
+        var chronID = '091e9c5e800d3b2d';
+        test.SearchFor(null, chronID, 'global search');
+        browser.pause(4000);
+        test.EditTheAsset();
+        act.ToggleAdditionalProperties();
+        var sslconfig = act.GetSSLConfig();
+        expect(sslconfig).to.be.false;
+        browser.pause(4000);
+        test.SaveOrPublishTheAsset('publish to live', 'Test');
+        browser.pause(10000);
+        checkATSStatus(assetChronID);
+        test.ClickButtonInATSPage("Redirect to URL");
+        handleRuntimeValidation('http');
+    });
+});
