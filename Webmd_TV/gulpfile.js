d3ed2e37d36f02f3bc720c242a14ded1dd09fd19
LSZ/sasank.tirumuneedi <stirumuneedi@webmd.net>
2017-08-04 16:45:03 +0530

    Committed

--- Webmd_TV/gulpfile.js
+++ Webmd_TV/gulpfile.js
@@ -6,27 +6,66 @@ var Launcher = require('webdriverio').Launcher;
 var chalk = require('chalk');
 var git = require('gulp-git');
 var gulpSequence = require('gulp-sequence');
-var env = "";
 var args = require("args");
 var glob = require("glob");
 var _ = require("lodash");
-var releaseconfig = require('./wdio.conf.js');
 var glob = require("glob");
+var fs = require("fs");
 
+args.option('env', 'Environment targetted', "PRODSEA1")
+    .option('branch', 'Master -- Will run all tests  branch/name (PPE-<branch name>) -- Will run branch tests  release release-sprint-<number>/integration runs sprint tests')
+    .option('maxInstances', 'Maximum number of instances a browser can have', 1)
+    .option('logLevel', 'Test runner logging level default error', 'error')
+    .option('selectTests', 'Tests to execute seperated by comma', 'not-selected');
 
-args.option('env', 'Environment targetted', "dev02")
-    .option('branch', 'Master -- Will run all tests  branch/name (PPE-<branch name>) -- Will run branch tests  release release-WebMDRx-<number>/integration runs sprint tests')
-    .option('samplesize', 'Sample Size', "10");
 var flags = args.parse(process.argv);
-//var configpath = `./config/${flags.env.toLowerCase()}.env`;
-//require('dotenv').config({ path: configpath });
+
+var testEnv = flags.env;
+var selectTest = flags.selectTests;
 var error = chalk.bold.red;
+var defaultWaitTimeout = 180000;
+var defaultMochaTestTimeout = 600000;
+var gridHost = '172.28.38.219';
+var gridPort = 4444;
+
 var tests = [];
-var excludeTests = [];
 var currentBranch;
-gulp.task('branch', function(cb) {
-    return git.revParse({ args: '--abbrev-ref HEAD' }, function(err, branch) {
-        console.log('current git branch: ' + branch);
+var confPath;
+var appFolder;
+/*switch (currentApp.toLowerCase()) {
+    case 'rt':
+        appFolder = 'rt';
+        break;
+    case 'd2cons':
+        appFolder = 'd2/cons';
+        break;
+    case 'd2prof':
+        appFolder = 'd2/prof';
+        break;
+    case 'pb2':
+        appFolder = 'pb2';
+        break;
+}*/
+    confPath = `./wdio.conf.js`;
+    conf = require(`./wdio.conf.js`);
+
+    testEnv = "PRODSEA1";
+
+gulp.task('branch', function (cb) {
+    return git.revParse({
+        args: '--abbrev-ref HEAD'
+    }, function (err, branch) {
+
+        //console.log('app: ' + currentApp);
+        console.log('branch: ' + branch);
+
+        if(testEnv.toLowerCase().indexOf('prod') >= 0)
+        {
+            console.log('PROD RUN NOT SUPPORTED FOR DEFAULT TASK');
+            cb();
+            return;
+        }
+
         if (branch === "HEAD" && flags.branch === undefined) {
             console.log(error('branch is required. Use --help to get detailed info'));
             process.exit(-1);
@@ -34,96 +73,165 @@ gulp.task('branch', function(cb) {
         if (branch === "HEAD") {
             branch = flags.branch;
         }
-        currentBranch = branch;
+        currentBranch = branch.toLowerCase();
+
+        var specBranch;
         if (currentBranch.indexOf('master') === 0) {
-            tests.push('jira/*.js');
-            tests.push('jira/**/*.test.js');
-            tests.push(`specs/**/*.js`);
-        } else if (currentBranch.indexOf('release-') >= 0) {
-            var testfile = currentBranch.toLowerCase().split("release-WebMDRx-")[1];
-            if (testfile) {
-                tests.push(`jira/rxsprint${testfile}/*.js`);
-                tests.push(`jira/rxsprint${testfile}/*.test.js`);
-                tests.push(`jira/rxrelease ${testfile}/*.js`);
-                tests.push(`jira/rxrelease ${testfile}/*.test.js`);
-                tests.push(`jira/rxhotfix ${testfile}/*.js`);
-                tests.push(`jira/rxhotfix ${testfile}/*.test.js`);
-            } else {
-                tests.push(`jira/**/*.js`);
-                tests.push(`jira/**/*.test.js`);
+            specBranch = `webmd-tv/fe/jira/*.js`;
+            console.log('master specs: ' + specBranch);
+        } 
+        else if (currentBranch.indexOf('release-') >= 0) {
+            var testfile = currentBranch.split("-");
+            specBranch = `test/${appFolder}/**/${testfile[testfile.length-1]}/*.js`;
+            console.log('release specs: ' + specBranch);
+        } else if (currentBranch.indexOf('integration-') >= 0) {
+            var testfile = currentBranch.split("-");
+            specBranch = `test/${appFolder}/**/${testfile[testfile.length-1]}/*.js`;
+            console.log('integration specs: ' + specBranch);
+        } else if (currentBranch.indexOf('ppe-') >= 0) {
+            var branchArr = currentBranch.split('-');
+            var ppeIndex = branchArr.indexOf('ppe');
+            if (ppeIndex >= 0) {
+                var testfile = branchArr[ppeIndex + 1];
+                specFiles = `test/${appFolder}/**/*${testfile}*.js`;
+				specFileFolder = `test/${appFolder}/**/*${testfile}*/*.js`;
+				tests.push(specFiles);
+				tests.push(specFileFolder);
+                console.log('feature ppe file specs: ' + specFiles);
+                console.log('feature ppe folder specs: ' + specFileFolder);
             }
-        } else if (currentBranch.indexOf('PPE-') >= 0) {
-            var testfile = currentBranch.toLowerCase().split("ppe-")[1].split("-")[0];
-            tests.push(`jira/**/${testfile}.js`);
-            tests.push(`jira/**/${testfile}.test.js`);
-        } else {
-            tests.push(`jira/**/*.js`);
-            tests.push(`jira/**/*.test.js`);
         }
+        if (specBranch) {
+            tests.push(specBranch);
+        }
+
         var results = [];
-        _.forEach(tests, function(t) {
+        _.forEach(tests, function (t) {
             if (!_.isEmpty(glob.sync(t))) {
                 results.push(t);
             }
         });
+
         if (results.length === 0) {
-            console.log("Found no pattern running all tests");
+            console.log("*** Found no pattern running all tests ***");
             tests = [];
-            tests.push(`baseline/*.test.js`);
-            tests.push(`jira/**/*.test.js`);
-            tests.push(`regression/*.test.js`);
-            tests.push(`smoke/*.test.js`);
-            console.log("Found no pattern running all tests", tests);
-        } else {
-            tests = results;
+            tests.push(`test/${appFolder}/**/baseline/**/*.js`);
+            tests.push(`test/${appFolder}/**/jira/**/*.js`);
+            tests.push(`test/${appFolder}/**/regression/**/*.js`);
+            tests.push(`test/${appFolder}/**/smoke/**/*.js`);
         }
-        console.log("Will run tests for ", tests);
         cb();
     });
 });
 
-gulp.task('allbranchs', function(cb) {
-    return git.revParse({ args: '--abbrev-ref HEAD' }, function(err, branch) {
-        console.log('current git branch: ' + branch);
-        //tests.push(`baseline/*.test.js`);
-        tests.push(`jira/**/*.js`);
-        //tests.push(`regression/*.test.js`);
-        //tests.push(`smoke/*.test.js`);
-        excludeTests.push('config/**');
-        excludeTests.push('helper/**');
-        excludeTests.push('pageobjects/**');
-        excludeTests.push('omniture/**');
-        excludeTests.push('jira/rxhotfix5-4-2017/**');
-        //excludeTests.push('jira/rxrelease2.4/**');
-        //excludeTests.push('jira/rxrelease2.5/**');
-        //excludeTests.push('jira/rxsprint4/**');
-        cb();
+var deleteFolderRecursive = function (path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+gulp.task('selected', function (done) {
+
+    console.log('selectTest: '+ selectTest);
+    if (selectTest.length > 0) {
+
+        if(selectTest.indexOf("," == 0))
+        {
+            selectTest += ",endtest";
+        }
+
+            _.each(selectTest.split(","), function (testId) {
+                var specFiles = `test/${appFolder}/**/*${testId.trim()}*.js`;
+                var specFileFolder = `test/${appFolder}/**/*${testId.trim()}*/*.js`;
+                tests.push(specFiles);
+                tests.push(specFileFolder);
+                console.log('selected file specs: ' + specFiles);
+                console.log('selected folder specs: ' + specFileFolder);
+            });
+    }
+    
+    conf.config.host = gridHost;
+    conf.config.port = gridPort;
+    gulpSequence('webdriver')(function (err) {
+        if (err) {
+            console.log('Failed: ' + err);
+        }
     });
+    done();
 });
 
+gulp.task('prod', function (done) {
+    tests.push(`test/${appFolder}/**/prod/**/*.js`);
+    conf.config.host = gridHost;
+    conf.config.port = gridPort;
+    gulpSequence('webdriver')(function (err) {
+        if (err){ console.log('Failed: ' + err); }
+    });
+    done();
+});
 
-gulp.task('webdriver', function(done) {
-    releaseconfig.config.specs = tests;
-    releaseconfig.config.exclude = excludeTests;
-    var wdio = new Launcher(path.join(__dirname, 'wdio.conf.js'), releaseconfig.config);
-    return wdio.run().then(function(code) {
-        console.log(code);
-    }, function(error) {
+gulp.task('smoke', function (done) {
+    tests.push(`test/${appFolder}/**/smoke/**/*.js`);
+    conf.config.host = gridHost;
+    conf.config.port = gridPort;
+    gulpSequence('webdriver')(function (err) {
+        if (err){ console.log('Failed: ' + err); }
+    });
+    done();
+});
+
+gulp.task('all', function (done) {
+    tests.push(`test/${appFolder}/**/baseline/**/*.js`);
+    tests.push(`test/${appFolder}/**/jira/**/*.js`);
+    tests.push(`test/${appFolder}/**/regression/**/*.js`);
+    tests.push(`test/${appFolder}/**/smoke/**/*.js`);
+    conf.config.host = gridHost;
+    conf.config.port = gridPort;
+    gulpSequence('webdriver')(function (err) {
+        if (err){ console.log('Failed: ' + err); }
+    });
+    done();
+});
+
+gulp.task('webdriver', function (done) {
+    conf.config.specs = tests;
+    conf.config.maxInstances = flags.maxInstances;
+    conf.config.logLevel = flags.logLevel;
+    conf.config.waitforTimeout = defaultWaitTimeout;
+    conf.config.mochaOpts.timeout = defaultMochaTestTimeout;
+
+    console.log('executing specs: ' + conf.config.specs.toString());
+    console.log('max instances: ' + conf.config.maxInstances);
+    console.log('log level: ' + conf.config.logLevel);
+    console.log('waitTimeout: ' + conf.config.waitforTimeout);
+    console.log('mocha test timeout: ' + conf.config.mochaOpts.timeout);
+    console.log('executing specs: ' + conf.config.specs.toString());
+
+    var wdio = new Launcher(path.join(__dirname, confPath), conf.config);
+    return wdio.run().then(function (code) {
+        console.log('wdio exit code: ' + code);
+    }, function (error) {
         console.error('Launcher failed to start the test', error.stacktrace);
         selenium.child.kill();
         process.exit(1);
     });
 });
 
-
-
-gulp.task('selenium', function(done) {
+gulp.task('selenium', function (done) {
     selenium.install({
-        logger: function(message) {}
-    }, function(err) {
+        logger: function (message) {}
+    }, function (err) {
         if (err) return done(err);
 
-        selenium.start(function(err, child) {
+        selenium.start(function (err, child) {
             if (err) return done(err);
             selenium.child = child;
             done();
@@ -131,41 +239,30 @@ gulp.task('selenium', function(done) {
     });
 });
 
-gulp.task('local', function(cb) {
-    gulpSequence('branch', 'selenium', 'webdriver')(function(err) {
+gulp.task('clean', function () {
+    var allure = rootPath = path.join(process.cwd(), "allure-results");
+    console.log('remove folder: ' + allure);
+    deleteFolderRecursive(allure);
+})
+
+gulp.task('local', function (cb) {
+    gulpSequence(['clean'], 'branch', 'selenium', 'webdriver')(function (err) {
         if (err) console.log(err)
         selenium.child.kill();
     });
 });
 
-gulp.task('e2e', function() {
-    releaseconfig.config.host = '172.28.38.219';
-    releaseconfig.config.port = 4444;
-    gulpSequence('branch', 'webdriver')(function(err) {
-        if (err) console.log(err);
+gulp.task('default', function (done) {
+    conf.config.host = gridHost;
+    conf.config.port = gridPort;
+    gulpSequence('branch', 'webdriver')(function (err) {
+        if (err){ console.log('Failed: ' + err); }
     });
-
+    done();
 });
 
-gulp.task('default', function() {
-    releaseconfig.config.host = '172.28.38.219';
-    releaseconfig.config.port = 4444;
-    gulpSequence('allbranchs', 'webdriver')(function(err) {
-        if (err) console.log(err);
-    });
-
-});
-
-gulp.task('test', function() {
-    releaseconfig.config.host = '172.28.38.219';
-    releaseconfig.config.port = 4444;
-    tests = [];
-    tests.push(`jira/RxHotFix5-4-2017/*.js`);
-    gulpSequence('selenium', 'webdriver')(function(err) {
-        if (err) console.log(err);
-    });
-
-});
-
-
-module.exports = "for giggles!";
+module.exports = {
+    TestEnv: testEnv.toLowerCase(),
+    MaxInstances: flags.maxInstances,
+    LogLevel: flags.logLevel,
+}
---
+++ Webmd_TV/gulpfiles.js
@@ -0,0 +1,172 @@
+var path = require('path');
+var gulp = require('gulp');
+var selenium = require('selenium-standalone');
+var webdriver = require('gulp-webdriver');
+var Launcher = require('webdriverio').Launcher;
+var chalk = require('chalk');
+var git = require('gulp-git');
+var gulpSequence = require('gulp-sequence');
+var env = "";
+var args = require("args");
+var glob = require("glob");
+var _ = require("lodash");
+var releaseconfig = require('./wdio.conf.js');
+var glob = require("glob");
+
+
+args.option('env', 'Environment targetted', "dev02")
+    .option('branch', 'Master -- Will run all tests  branch/name (PPE-<branch name>) -- Will run branch tests  release release-WebMDRx-<number>/integration runs sprint tests')
+    .option('samplesize', 'Sample Size', "10");
+var flags = args.parse(process.argv);
+//var configpath = `./config/${flags.env.toLowerCase()}.env`;
+//require('dotenv').config({ path: configpath });
+var error = chalk.bold.red;
+var tests = [];
+var excludeTests = [];
+var currentBranch;
+gulp.task('branch', function(cb) {
+    return git.revParse({ args: '--abbrev-ref HEAD' }, function(err, branch) {
+        console.log('current git branch: ' + branch);
+        if (branch === "HEAD" && flags.branch === undefined) {
+            console.log(error('branch is required. Use --help to get detailed info'));
+            process.exit(-1);
+        }
+        if (branch === "HEAD") {
+            branch = flags.branch;
+        }
+        currentBranch = branch;
+        if (currentBranch.indexOf('master') === 0) {
+            tests.push('jira/*.js');
+            tests.push('jira/**/*.test.js');
+            tests.push(`specs/**/*.js`);
+        } else if (currentBranch.indexOf('release-') >= 0) {
+            var testfile = currentBranch.toLowerCase().split("release-WebMDRx-")[1];
+            if (testfile) {
+                tests.push(`jira/rxsprint${testfile}/*.js`);
+                tests.push(`jira/rxsprint${testfile}/*.test.js`);
+                tests.push(`jira/rxrelease ${testfile}/*.js`);
+                tests.push(`jira/rxrelease ${testfile}/*.test.js`);
+                tests.push(`jira/rxhotfix ${testfile}/*.js`);
+                tests.push(`jira/rxhotfix ${testfile}/*.test.js`);
+            } else {
+                tests.push(`jira/**/*.js`);
+                tests.push(`jira/**/*.test.js`);
+            }
+        } else if (currentBranch.indexOf('PPE-') >= 0) {
+            var testfile = currentBranch.toLowerCase().split("ppe-")[1].split("-")[0];
+            //tests.push(`jira/**/${testfile}.js`);
+			tests.push(`jira/*.js');
+            tests.push(`jira/**/${testfile}.test.js`);
+        } else {
+            tests.push(jira/*.js);
+            tests.push(`jira/**/*.test.js`);
+        }
+        var results = [];
+        _.forEach(tests, function(t) {
+            if (!_.isEmpty(glob.sync(t))) {
+                results.push(t);
+            }
+        });
+        if (results.length === 0) {
+            console.log("Found no pattern running all tests");
+            tests = [];
+            tests.push(`baseline/*.test.js`);
+            tests.push(`jira/**/*.test.js`);
+            tests.push(`regression/*.test.js`);
+            tests.push(`smoke/*.test.js`);
+            console.log("Found no pattern running all tests", tests);
+        } else {
+            tests = results;
+        }
+        console.log("Will run tests for ", tests);
+        cb();
+    });
+});
+
+gulp.task('allbranchs', function(cb) {
+    return git.revParse({ args: '--abbrev-ref HEAD' }, function(err, branch) {
+        console.log('current git branch: ' + branch);
+        //tests.push(`baseline/*.test.js`);
+        tests.push(`jira/**/*.js`);
+        //tests.push(`regression/*.test.js`);
+        //tests.push(`smoke/*.test.js`);
+        excludeTests.push('config/**');
+        excludeTests.push('helper/**');
+        excludeTests.push('pageobjects/**');
+        excludeTests.push('omniture/**');
+        excludeTests.push('jira/rxhotfix5-4-2017/**');
+        //excludeTests.push('jira/rxrelease2.4/**');
+        //excludeTests.push('jira/rxrelease2.5/**');
+        //excludeTests.push('jira/rxsprint4/**');
+        cb();
+    });
+});
+
+
+gulp.task('webdriver', function(done) {
+    releaseconfig.config.specs = tests;
+    releaseconfig.config.exclude = excludeTests;
+    var wdio = new Launcher(path.join(__dirname, 'wdio.conf.js'), releaseconfig.config);
+    return wdio.run().then(function(code) {
+        console.log(code);
+    }, function(error) {
+        console.error('Launcher failed to start the test', error.stacktrace);
+        selenium.child.kill();
+        process.exit(1);
+    });
+});
+
+
+
+gulp.task('selenium', function(done) {
+    selenium.install({
+        logger: function(message) {}
+    }, function(err) {
+        if (err) return done(err);
+
+        selenium.start(function(err, child) {
+            if (err) return done(err);
+            selenium.child = child;
+            done();
+        });
+    });
+});
+
+gulp.task('local', function(cb) {
+    gulpSequence('branch', 'selenium', 'webdriver')(function(err) {
+        if (err) console.log(err)
+        selenium.child.kill();
+    });
+});
+
+gulp.task('e2e', function() {
+    releaseconfig.config.host = '172.28.38.219';
+    releaseconfig.config.port = 4444;
+    gulpSequence('branch', 'webdriver')(function(err) {
+        if (err) console.log(err);
+    });
+
+});
+
+gulp.task('default', function() {
+    releaseconfig.config.host = '172.28.38.219';
+    releaseconfig.config.port = 4444;
+    gulpSequence('allbranchs', 'webdriver')(function(err) {
+        if (err) console.log(err);
+    });
+
+});
+
+gulp.task('test', function() {
+    releaseconfig.config.host = '172.28.38.219';
+    releaseconfig.config.port = 4444;
+    tests = [];
+    tests.push(`jira/RxHotFix5-4-2017/*.js`);
+    gulpSequence('selenium', 'webdriver')(function(err) {
+        if (err) console.log(err);
+    });
+
+});
+
+
+module.exports = "for giggles!";
