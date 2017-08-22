2e302817820f7e3bd4d89c0c86fba1e6b0789453
Bhaskar Sistla <bsistla@webmd.net>
2017-08-22 20:20:11 +0530

    PPE-106531: Added Programming user credentials and dpm module data.(TestCases:PPE-126076, PPE-126077, PPE-126886, PPE-126887)

--- test/pb2/data/pagemodule.assets.js
+++ test/pb2/data/pagemodule.assets.js
@@ -18,7 +18,29 @@ var pmdata = Object.create(page, {
             "linkedModule":null,
             "sponsorProgram": null,
             "description": moduleName + "-desc",
-            "tier": "tier2",
+            "tier": 2,
+        }
+        }}
+        },
+
+        htmlDPModule: {value: {get : (moduleName) => {
+        var pmProps = {};
+        return pmProps  = 
+        {
+            "moduleName": moduleName,
+            "moduleDispName": moduleName,
+            "moduleType": "HTML",
+            "category": null,
+            "selectXSL": "HTML",
+            "selectCSS": null,
+            "dynamicModuleCategory": "Channel Home",
+            "isDefault": true,
+            "moduleLabel1":null,
+            "moduleLabel2":null,
+            "linkedModule":null,
+            "sponsorProgram": null,
+            "description": moduleName + "-desc",
+            "tier": 2,
 
         }
     }}
