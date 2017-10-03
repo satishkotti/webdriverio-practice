b09ad85019fa2df06c7ffed0e185348392a93bb1
Bhaskar Sistla <bsistla@webmd.net>
2017-10-03 16:32:16 +0530

    PPE-106109: Removed encrypting code.(TestCases:PPE-131436, PPE-131437)

--- test/pb2/fe/jira/release_2.12/ppe-106109/ppe-106109.js
+++ test/pb2/fe/jira/release_2.12/ppe-106109/ppe-106109.js
@@ -5,12 +5,6 @@ var chronid = null;
 var xmlFile = null;
 var pb2Session = null;
 
-function encrypt(ele){
-    ele = ele.replace(/</g, '&lt;')
-    ele = ele.replace(/>/g, '&gt;')
-    return ele;
-}
-
 
 describe('HTML code must be retained by the UI text fields', () => {
 
@@ -21,47 +15,42 @@ describe('HTML code must be retained by the UI text fields', () => {
             test.ConfigureModule('Standard Promo', testdata.create);
             test.SaveOrPublishTheAsset('Publish to Live', 'Testing PPE-106109 using automation script');
             browser.pause(5000);
+            console.log("Asset Chronicle ID: ", chronid)
         })
 
         it('HTML code must be reatined by Module Title Text field', () => {
             var actual = browser.execute(`return $('label:contains("Module Title:") input')[0].value`).value;
             var expected = testdata.create.ModuleTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be reatined by Logo Title Text field', () => {
             var actual = browser.execute(`return $('label:contains("Logo Title:") input')[0].value`).value;
             var expected = testdata.create.LogoTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be reatined by Slide Header Text field', () => {
             var actual = browser.execute(`return $('label:contains("Slide Header Text:") input')[0].value`).value;
             var expected = testdata.create.Slides[0].SlideHeaderText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be reatined by Slide Title Text field', () => {
             var actual = browser.execute(`return $('label:contains("Slide Title:") input')[0].value`).value;
             var expected = testdata.create.Slides[0].SlideTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be reatined by Slide Emphasized Text field', () => {
             var actual = browser.execute(`return $('label:contains("Slide Emphasized Text:") input')[0].value`).value;
             var expected = testdata.create.Slides[0].SlideEmphasizedText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be reatined by Slide Sub Text Text field', () => {
             var actual = browser.execute(`return $('label:contains("Slide Sub Text:") input')[0].value`).value;
             var expected = testdata.create.Slides[0].SlideSubText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 });
@@ -79,42 +68,36 @@ describe('HTML code must be encoded and retained in the XML', () => {
         it('HTML code must be retained in encoded format for module_title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.module_title.title;
             var expected = testdata.create.ModuleTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be retained in encoded format for logo title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.logos.logo.logo_overridetext;
             var expected = testdata.create.LogoTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be retained in encoded format for slide header text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_name;
             var expected =testdata.create.Slides[0].SlideHeaderText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be retained in encoded format for slide title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_title;
             var expected = testdata.create.Slides[0].SlideTitle;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be retained in encoded format for slide emphasized text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_title_emphasized_text;
             var expected = testdata.create.Slides[0].SlideEmphasizedText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 
         it('HTML code must be retained in encoded format for slide sub text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_sub_text;
             var expected = testdata.create.Slides[0].SlideSubText;
-            expected = encrypt(expected);
             expect(actual).to.equal(expected);
         });
 });
@@ -181,37 +164,37 @@ describe('$1 must be encoded and retained in the XML', () => {
 
         it('$1 must be retained in encoded format for module_title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.module_title.title;
-            var expected = encodeURIComponent(testdata.edit.ModuleTitle);
+            var expected = testdata.edit.ModuleTitle;
             expect(actual).to.equal(expected);
         });
 
         it('$1 must be retained in encoded format for logo title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.logos.logo.logo_overridetext;
-            var expected = encodeURIComponent(testdata.edit.LogoTitle);
+            var expected = testdata.edit.LogoTitle;
             expect(actual).to.equal(expected);
         });
 
         it('$1 must be retained in encoded format for slide header text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_name;
-            var expected = encodeURIComponent(testdata.edit.Slides[0].SlideHeaderText);
+            var expected = testdata.edit.Slides[0].SlideHeaderText;
             expect(actual).to.equal(expected);
         });
 
         it('$1 must be retained in encoded format for slide title tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_title;
-            var expected = encodeURIComponent(testdata.edit.Slides[0].SlideTitle);
+            var expected = testdata.edit.Slides[0].SlideTitle;
             expect(actual).to.equal(expected);
         });
 
         it('$1 must be retained in encoded format for slide emphasized text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_title_emphasized_text;
-            var expected = encodeURIComponent(testdata.edit.Slides[0].SlideEmphasizedText);
+            var expected = testdata.edit.Slides[0].SlideEmphasizedText;
             expect(actual).to.equal(expected);
         });
 
         it('$1 must be retained in encoded format for slide sub text tag in the XML', () => {
             var actual = xmlFile.webmd_rendition.content.wbmd_asset.webmd_module.module_data.slides.slide[0].slide_sub_text;
-            var expected = encodeURIComponent(testdata.edit.Slides[0].SlideSubText);
+            var expected = testdata.edit.Slides[0].SlideSubText;
             expect(actual).to.equal(expected);
         });
 });
