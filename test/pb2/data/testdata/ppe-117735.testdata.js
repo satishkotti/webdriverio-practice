1b08bbc544e3ec71a919dba3257f2b89e7946ba1
Bhaskar Sistla <bsistla@webmd.net>
2017-10-05 19:24:36 +0530

    PPE-117735: Added Test cases.(TestCases:PPE-133030, PPE-133031, PPE-133032)

---
+++ test/pb2/data/testdata/ppe-117735.testdata.js
@@ -0,0 +1,37 @@
+const randomstring = require('randomstring');
+var assetName = `QASM-PPE117735-${randomstring.generate(5)}`;
+
+module.exports = {
+    ppe_117735:
+    {
+            "moduleName": assetName,
+            "moduleDispName": assetName,
+            "moduleType": "StandardPromo",
+            "category": null,
+            "selectXSL": null,
+            "selectCSS": null,
+            "dynamicModuleCategory": null,
+            "moduleLabel1": null,
+            "moduleLabel2": null,
+            "linkedModule": null,
+            "sponsorProgram": null,
+            "description": assetName + "-desc",
+            "tier": 2,
+            "ModuleTitle": "<title>Title</title>",
+            "ModuleLink": "091e9c5e801651b2",
+            "LogoImage": "091e9c5e803fecd4",
+            "LogoTitle": "<logotitle>logotitle</logotitle>",
+            "LogoLink": "091e9c5e80661dc9",
+            "Slides":
+            [
+                {
+                    "Image":"091e9c5e803fecd4",
+                    "SlideHeaderText": "<slideheadertext>SlideHeaderText</slideheadertext>",
+                    "SlideHeaderLink": "091e9c5e80661dc9",
+                    "SlideTitle": "<slidetitle>SlideTitle</slidetitle>",
+                    "SlideEmphasizedText": "<slideemphasizedtext>SlideEmphasizedText</slideemphasizedtext>",
+                    "SlideSubText": "<slidesubtext>SlideSubText</slidesubtext>"
+                }
+            ],
+    }
+}
