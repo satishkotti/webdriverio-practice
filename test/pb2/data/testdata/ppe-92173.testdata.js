4fcfaf9d2844bb7105286fdaea578b03a21fecaa
uburman <uburman@webmd.net>
2017-10-18 10:10:09 -0400

    Test cases for PPE-92173 &  PPE-82827

---
+++ test/pb2/data/testdata/ppe-92173.testdata.js
@@ -0,0 +1,12 @@
+module.exports = {
+    RemovePB1Links_ActionLinks: {
+        "Preferences":'//li[contains(.,"Preferences")]',
+        "Refresh_PB1_Caches":'//li[contains(.,"Refresh PB1 Caches")]',
+        "Reset_PB1_Sessions":'//li[contains(.,"Reset PB1 sessions")]',
+        "Change_Site":'//li[contains(.,"Change Site")]',
+        "PageBuilder_Help":'//a[contains(.,"PageBuilder Help")]',
+        "PublishNode" :'//li[contains(.,"Publish Node")]',
+        "Logout" :'//li[contains(.,"Logout")]'
+    },
+
+}
