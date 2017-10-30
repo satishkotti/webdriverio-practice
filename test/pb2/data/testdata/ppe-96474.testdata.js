19d38506856a1a26c2f1a77a852f2040c97ba715
Aishwarya Donthula <aishwarya.donthula@valuelabs.com>
2017-10-30 18:19:51 +0530

    PPE-96474-Updated testdata and testcase

--- test/pb2/data/testdata/ppe-96474.testdata.js
+++ test/pb2/data/testdata/ppe-96474.testdata.js
@@ -1,43 +1,100 @@
 module.exports = {
-    subdomainurls:
+    validsubdomainurls:
     [
         
         {
-            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/teen/food/article/bullying-survival-skills',
+            "url": 'https://exchanges$$LifeCycle$$$$CoreDomain$$/webmd-exchanges/experts-guests-sponsors',
             "siteId": "3",
             "environment": global.testEnv
         },
         {
-            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/teen/food/article/kiwano-melon',
+            "url": 'https://relief$$LifeCycle$$$$CoreDomain$$/allergies/relief-advisor',
             "siteId": "3",
             "environment": global.testEnv
         },
         {
-            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/teen/food/article/feel-better-about-body',
+            "url": 'https://relief-staging$$LifeCycle$$$$CoreDomain$$/allergies/relief-advisor',
             "siteId": "3",
             "environment": global.testEnv
         },
         {
-            "url": 'https://pets$$LifeCycle$$$$CoreDomain$$/ask-pet-health-11/cat-nutrition',
+            "url": 'https://symptomsbeta$$LifeCycle$$$$CoreDomain$$/default.htm',
             "siteId": "3",
             "environment": global.testEnv
         },
         {
-            "url": 'https://pets$$LifeCycle$$$$CoreDomain$$/ask-pet-health-11/cat-training',
+            "url": 'https://dictionary$$LifeCycle$$$$CoreDomain$$/default.htm',
             "siteId": "3",
             "environment": global.testEnv
         },
         {
-            "url": 'https://pets$$LifeCycle$$$$CoreDomain$$/ask-pet-health-11/cat-training',
+            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/editorial-policy',
             "siteId": "3",
             "environment": global.testEnv
         },
 
         {
-            "url": 'https://pets$$LifeCycle$$$$CoreDomain$$/ask-pet-health-11/dog-feeding',
+            "url": 'https://exchanges$$LifeCycle$$$$CoreDomain$$/webmd-exchanges/blogsrssbelowthebeltwomenshealth',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+         {
+            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/kids/food/article/default.htm',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        {
+            "url": 'https://fit$$LifeCycle$$$$CoreDomain$$/kids/food/eval/your-fit-food-horoscope',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+         {
+            "url": 'https://pets$$LifeCycle$$$$CoreDomain$$/healthy-pets-vet-16/how-often-bathe-your-dog-poll',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        {
+            "url": 'https://symptoms$$LifeCycle$$$$CoreDomain$$/seasonal-allergy-map-tool/allergy-free-gardening',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        {
+            "url": 'https://symptoms$$LifeCycle$$$$CoreDomain$$/seasonal-allergy-map-tool/default.htm',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        {
+            "url": 'https://teens$$LifeCycle$$$$CoreDomain$$/girls-puberty-10/default.htm',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+         {
+            "url": 'https://teens$$LifeCycle$$$$CoreDomain$$/virtual/news/default.htm',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+         {
+            "url": 'https://www$$LifeCycle$$$$CoreDomain$$/a-to-z-guides/healthy-living/e.htm',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        ],
+
+        invalidsubdomainurls:
+    [
+        
+        {
+            "url": 'https://exchanges$$LifeCycle$$$$CoreDomain$$/teen/food-exchanges-health',
+            "siteId": "3",
+            "environment": global.testEnv
+        },
+        {
+            "url": 'https://teens$$LifeCycle$$$$CoreDomain$$/exchanges-food',
             "siteId": "3",
             "environment": global.testEnv
         }
+        
         ]
+
 
 }
--- test/pb2/fe/jira/release_2.13/ppe-96474/ppe-96474.js
+++ test/pb2/fe/jira/release_2.13/ppe-96474/ppe-96474.js
@@ -6,37 +6,73 @@ var accessToken,response;
 describe('PPE-96474', function() {
      
 
-   it('Search valid subdomain urls and should return chronicle id with SearchByFriendlyurl',function() {
+ /*  it('Search valid subdomain urls and should return chronicle id with SearchByFriendlyurl',function() {
        accessToken = test.GenerateApiAccessToken();
-    var urls = testdata.subdomainurls;
+    var urls = testdata.validsubdomainurls;
      urls.forEach(function (asset) {
          var browseurl;
-         var testUrl = asset.url.replace("$$LifeCycle$$", ".preview.");
-         if (global.testEnv == "prodsea1") {
+         var testUrl = asset.url.replace("$$LifeCycle$$",".");
+         if (test.IsProductionEnv == true) {
              browseurl = testUrl.replace("$$CoreDomain$$", "webmd.com")
          }
          else {
              browseurl = testUrl.replace("$$CoreDomain$$", "" + global.testEnv + ".webmd.com")
          }
+         asset.url = browseurl;
          response = test.searchurl(accessToken, asset)
          expect(response.Data).to.not.equal(null);
      }, this);
    })
     it('Search valid subdomain urls and should return chronicle id with SearchByFriendlyurl',function() {
        accessToken = test.GenerateApiAccessToken();
-    var urls = testdata.subdomainurls;
+    var urls = testdata.validsubdomainurls;
      urls.forEach(function (asset) {
          var browseurl;
          var testUrl = asset.url.replace("$$LifeCycle$$", ".preview.");
-         if (global.testEnv == "prodsea1") {
+         if (test.IsProductionEnv == true) {
              browseurl = testUrl.replace("$$CoreDomain$$", "webmd.com")
          }
          else {
              browseurl = testUrl.replace("$$CoreDomain$$", "" + global.testEnv + ".webmd.com")
          }
+         asset.url = browseurl;
          response = test.searchbaseurl(accessToken, asset)
          expect(response.Data).to.not.equal(null);
      }, this);
+   })*/
+   it('Search invalid subdomain urls and should not return chronicle id with SearchByFriendlyurl',function() {
+       accessToken = test.GenerateApiAccessToken();
+    var urls = testdata.invalidsubdomainurls;
+     urls.forEach(function (asset) {
+         var browseurl;
+         var testUrl = asset.url.replace("$$LifeCycle$$", ".");
+         if (test.IsProductionEnv == true) {
+             browseurl = testUrl.replace("$$CoreDomain$$", "webmd.com")
+         }
+         else {
+             browseurl = testUrl.replace("$$CoreDomain$$", "" + global.testEnv + ".webmd.com")
+         }
+         asset.url = browseurl;
+         response = test.searchbaseurl(accessToken, asset)
+         expect(response.Data).to.equal(null);
+     }, this);
+   })
+   it('Search invalid subdomain urls and should not return chronicle id with SearchByFriendlyurl',function() {
+       accessToken = test.GenerateApiAccessToken();
+    var urls = testdata.invalidsubdomainurls;
+     urls.forEach(function (asset) {
+         var browseurl;
+         var testUrl = asset.url.replace("$$LifeCycle$$", ".");
+         if (test.IsProductionEnv == true) {
+             browseurl = testUrl.replace("$$CoreDomain$$", "webmd.com")
+         }
+         else {
+             browseurl = testUrl.replace("$$CoreDomain$$", "" + global.testEnv + ".webmd.com")
+         }
+         asset.url = browseurl;
+         response = test.searchurl(accessToken, asset)
+         expect(response.Data).to.equal(null);
+     }, this);
+   });
    })
     
-})
